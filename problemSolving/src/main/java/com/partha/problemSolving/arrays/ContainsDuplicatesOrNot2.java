a604704db02f8701526c6ffd42c2a47b75794702
partha011 <biswas.partha1@gmail.com>
2019-02-20 20:32:43 +0530

    problem solving : array rotation and duplicates

---
+++ problemSolving/src/main/java/com/partha/problemSolving/arrays/ContainsDuplicatesOrNot2.java
@@ -0,0 +1,28 @@
+package com.partha.problemSolving.arrays;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicatesOrNot2 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[]{23,12,45,2,12};
+		System.out.println(containsDuplicates(arr));
+		
+	}
+
+	public static boolean containsDuplicates(int[] arr){
+		boolean result=false;
+		Set<Integer> set = new HashSet<>();
+		for(int i=0;i<arr.length;i++){
+			if(set.contains(arr[i])){
+				result = true;
+				break;
+			}else{
+				set.add(arr[i]);
+			}
+		}
+		return result;
+	}
+
+}
