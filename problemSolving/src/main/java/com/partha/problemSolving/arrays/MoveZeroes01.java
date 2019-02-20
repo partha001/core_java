921f3af717065bfb5218e63f5a7bfacf1d3bac29
Partha Biswas <biswas.partha1@gmail.com>
2019-02-20 14:30:16 +0530

    problem solving : moving alll zeroes to the end of the array

--- problemSolving/src/main/java/com/partha/problemSolving/arrays/MoveZeroes01.java
+++ problemSolving/src/main/java/com/partha/problemSolving/arrays/MoveZeroes01.java
@@ -12,7 +12,7 @@ import java.util.stream.IntStream;
 public class MoveZeroes01 {
 
 	public static void main(String[] args) {
-		int[] arr= new int[]{0,1,0,3,12};
+		int[] arr= new int[]{4,0,3,0,2};
 		 int n = arr.length; 
 	     pushZerosToEnd(arr, n); 
 	     IntStream.range(0, n).forEach(i -> {
@@ -24,21 +24,21 @@ public class MoveZeroes01 {
 	
     static void pushZerosToEnd(int arr[], int n) 
     { 
-        int count = 0;  // Count of non-zero elements 
-  
-        // Traverse the array. If element encountered is 
-        // non-zero, then replace the element at index 'count' 
-        // with this element 
-        for (int i = 0; i < n; i++) 
-            if (arr[i] != 0) 
-                arr[count++] = arr[i]; // here count is 
-                                       // incremented 
-  
-        // Now all non-zero elements have been shifted to 
-        // front and 'count' is set as index of first 0. 
-        // Make all elements 0 from count to end. 
-        while (count < n) 
-            arr[count++] = 0; 
+    	  int count = 0;  // Count of non-zero elements 
+    	  
+          // Traverse the array. If element encountered is 
+          // non-zero, then replace the element at index 'count' 
+          // with this element 
+          for (int i = 0; i < n; i++) 
+              if (arr[i] != 0) 
+                  arr[count++] = arr[i]; // here count is 
+                                         // incremented 
+    
+          // Now all non-zero elements have been shifted to 
+          // front and 'count' is set as index of first 0. 
+          // Make all elements 0 from count to end. 
+          while (count < n) 
+              arr[count++] = 0; 
     } 
   
 
