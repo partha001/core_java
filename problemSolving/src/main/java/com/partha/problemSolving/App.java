5c6f0c501d0bee6f88e1692ef815486b695d8bc2
partha011 <biswas.partha1@gmail.com>
2019-03-17 13:50:37 +0530

    problem solving

--- problemSolving/src/main/java/com/partha/problemSolving/App.java
+++ problemSolving/src/main/java/com/partha/problemSolving/App.java
@@ -11,3 +11,7 @@ public class App
         System.out.println( "Hello World!" );
     }
 }
+
+
+
+
--- problemSolving/src/main/java/com/partha/problemSolving/linkedlists/ReversingASinglyLinkedList.java
+++ problemSolving/src/main/java/com/partha/problemSolving/linkedlists/ReversingASinglyLinkedList.java
@@ -9,14 +9,39 @@ public class ReversingASinglyLinkedList {
 		list.head = new ReversingASinglyLinkedList.Node(10);
 		list.head.next = new  ReversingASinglyLinkedList.Node(20);
 		list.head.next.next = new  ReversingASinglyLinkedList.Node(30);
-		list.printList();
+		printList(list);
 		
-		
+		printList(reversingLinkedList(list));
 		
 		
 	}
 	
-
+	public static LinkedList reversingLinkedList(LinkedList  list){
+		LinkedList reversedLinkedList = new LinkedList();
+		Node current = list.head;
+		while(current!=null){
+			Node newNode = null;
+			if(reversedLinkedList.head == null){
+				newNode = new Node(current.data);
+				reversedLinkedList.head = newNode;
+			}else{
+				newNode = new Node(current.data);
+				newNode.next = reversedLinkedList.head;
+				reversedLinkedList.head = newNode;
+			}
+			current = current.next;
+		}
+		return reversedLinkedList;
+	}
+	
+	public static void printList(LinkedList list){
+		System.out.println("");
+		ReversingASinglyLinkedList.Node current = list.head;
+		while(current!=null){
+			System.out.print(current.data + " -> ");
+			current = current.next;
+		}
+	}
 	
 	
 	static class LinkedList{
@@ -31,21 +56,6 @@ public class ReversingASinglyLinkedList {
 			Node node = new Node();
 			node.data = data;
 			node.next = null;
-		}
-		
-		public LinkedList reversingLinkedList(){
-			LinkedList reversedLinkedList = new 
-			
-		}
-		
-		
-		public void printList(){
-			System.out.println("");
-			ReversingASinglyLinkedList.Node current = this.head;
-			while(current!=null){
-				System.out.print(current.data + " -> ");
-				current = current.next;
-			}
 		}
 		
 		
