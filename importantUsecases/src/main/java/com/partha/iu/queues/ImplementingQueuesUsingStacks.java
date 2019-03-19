027a6271ed9dd4ba0ade63576e0d0879676d5d6b
partha011 <biswas.partha1@gmail.com>
2019-03-19 19:30:26 +0530

    important usecase

---
+++ importantUsecases/src/main/java/com/partha/iu/queues/ImplementingQueuesUsingStacks.java
@@ -0,0 +1,13 @@
+package com.partha.iu.queues;
+
+import java.util.LinkedList;
+
+public class ImplementingQueuesUsingStacks {
+
+	public static void main(String[] args) {
+LinkedList<Integer> list = new LinkedList<Integer>();
+list.add(10);
+list.add(20);
+	}
+
+}
