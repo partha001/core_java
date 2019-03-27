9fef7c22288bc87402ba2bb4beaa979f6268e5bd
partha011 <biswas.partha1@gmail.com>
2019-03-27 17:28:12 +0530

    binary search tree

--- datastructure01/src/main/java/com/partha/ds/tree/binaryTree01/practice/BinarySearchTreeExample1.java
+++ datastructure01/src/main/java/com/partha/ds/tree/binaryTree01/practice/BinarySearchTreeExample1.java
@@ -1,39 +1,27 @@
 package com.partha.ds.tree.binaryTree01.practice;
 
 public class BinarySearchTreeExample1 {
-	
+
 	public static void main(String[] args){
-		
+
 	}
-	
-	
-	class BinarySearchTree{
-		Node root;
-		
-		BinarySearchTree(){
-			super();
-		}
-		
-		BinarySearchTree(Node node){
-			super();
-			this.root=node;
-		}
-		
-	}
-	
-	
-	 class Node{
-		 
-		 int data;
-		 Node leftChild;
-		 Node rightChild;
-		 
+
+
+	class BinarySearchTree{}
+
+
+	class Node{
+
+		int data;
+		Node leftChild;
+		Node rightChild;
+
 		public Node(int data) {
 			super();
 			this.data = data;
 		}
-		
-	 }
+
+	}
 
 }
 
