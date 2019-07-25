c0b44544e4c19797d1608475e4b7816160e54359
partha011 <biswas.partha1@gmail.com>
2019-07-26 03:30:25 +0530

    multithreading : executor framework

---
+++ multithreading/src/main/java/com/partha/multithread/topic08/executors/fixedThreadPoolExecutor01/Application01.java
@@ -0,0 +1,36 @@
+package com.partha.multithread.topic08.executors.fixedThreadPoolExecutor01;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Application01 {
+
+	public static void main(String[] args) {
+		System.out.println("starting main thread");
+		ExecutorService executor = Executors.newFixedThreadPool(1);
+		executor.submit(new Thread(new MyThread(), "thread1"));
+		System.out.println("completing main thread");
+	}
+	
+	
+	static class MyThread implements Runnable{
+
+		@Override
+		public void run() {
+			System.out.println("thread:"+ Thread.currentThread().getName()+ " started");
+			for(int i=0;i<5;i++){
+				try {
+					Thread.currentThread().sleep(1000);
+					System.out.println("thread:"+Thread.currentThread().getName()+ "  "+i);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+				
+			System.out.println("thread:"+ Thread.currentThread().getName()+ " completed");
+		}
+		
+	}
+
+}
