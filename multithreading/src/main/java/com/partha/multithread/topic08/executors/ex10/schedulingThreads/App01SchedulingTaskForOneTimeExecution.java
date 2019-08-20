d619797aa5bfc87791b0d77847c82d5b09bcc7d2
Partha Biswas <biswas.partha1@gmail.com>
2019-08-20 20:29:07 +0530

    multithreading

---
+++ multithreading/src/main/java/com/partha/multithread/topic08/executors/ex10/schedulingThreads/App01SchedulingTaskForOneTimeExecution.java
@@ -0,0 +1,9 @@
+package com.partha.multithread.topic08.executors.ex10.schedulingThreads;
+
+public class App01SchedulingTaskForOneTimeExecution {
+
+	public static void main(String[] args) {
+
+	}
+
+}
