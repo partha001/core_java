f656bab6ec80025667b3d7298f71ca33ab561cce
Partha Biswas <biswas.partha1@gmail.com>
2019-09-11 16:03:55 +0530

    problem solving

---
+++ problemSolving/src/main/java/com/partha/problemSolving/strings/Solution1.java
@@ -0,0 +1,57 @@
+package com.partha.problemSolving.strings;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution1 {
+
+	public static void main(String[] args) {
+		processStrings("ABC","BC");
+		processStrings("BC","BANGALORE");
+
+	}
+
+	/**
+	 * the below is the required solution method
+	 * @param str1
+	 * @param str2
+	 * @return
+	 */
+	public static List<String> processStrings(String str1,String str2){
+		List<String> result = new ArrayList<String>();
+		String s1 = "";
+		String s2 = "";
+		Set<Character> set = new LinkedHashSet<Character>();
+		
+		for(Character c: str1.toCharArray()){
+			set.add(c);
+		}
+		for(Character c:str2.toCharArray()){
+			set.remove(c);
+		}
+		for(Character c:set){
+			s1+=c;
+		}
+		
+		set.clear();
+		for(Character c: str2.toCharArray()){
+			set.add(c);
+		}
+		for(Character c:str1.toCharArray()){
+			set.remove(c);
+		}
+		for(Character c:set){
+			s2+=c;
+		}
+
+		result.add(s1);
+		result.add(s2);
+
+
+		return result;
+
+	}
+
+}
