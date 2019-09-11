ca084515092c018f80c3ea739be5f148067fdc4c
partha011 <biswas.partha1@gmail.com>
2019-09-11 12:09:58 +0530

    memory management

---
+++ memoryManagement/src/main/java/com/partha/mm/example01/Application01.java
@@ -0,0 +1,21 @@
+package com.partha.mm.example01;
+
+/**
+ * this example shows that primitive values are always pass by value
+ * @author parbiswa
+ *
+ */
+public class Application01 {
+
+	public static void main(String[] args) {
+		int var = 10;
+		incrementByOneHundred(var);
+		System.out.println(var);
+		
+	}
+	
+	public static void incrementByOneHundred(int var){
+		var =  var + 100;
+	}
+
+}
