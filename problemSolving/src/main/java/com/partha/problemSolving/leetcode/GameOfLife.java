66a958edfb1b9779cb4cdc9c482a31ba95dc490e
Partha Biswas <biswas.partha1@gmail.com>
2021-03-26 02:00:40 +0530

    game of life,maximum product subarray

---
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/GameOfLife.java
@@ -0,0 +1,87 @@
+package com.partha.problemSolving.leetcode;
+
+/**
+ * https://leetcode.com/problems/game-of-life/
+ * @author partha
+ *
+ */
+public class GameOfLife {
+
+	public static void main(String[] args) {
+
+	}
+
+
+	/**
+	 * 
+	 * @author partha
+	 *
+	 */
+	private static class Solution1 {
+		// Using named constants for readability
+		private final static int IS_ALIVE = 1;
+		private final static int IS_DEAD = 0;    
+		private final static int WAS_ALIVE_NOW_DEAD = 2;
+		private final static int WAS_DEAD_NOW_ALIVE = 3;
+
+		public void gameOfLife(int[][] board) {
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[i].length; j++) {
+					int live = countNeighbors(i, j, board, IS_ALIVE);
+					int dead = countNeighbors(i, j, board, IS_DEAD);
+					board[i][j] = updatedStatus(getExistingState(board[i][j]), live, dead);
+				}
+			}
+			for (int i = 0; i < board.length; i++) {
+				for (int j = 0; j < board[i].length; j++) {
+					if (board[i][j] == WAS_ALIVE_NOW_DEAD) board[i][j] = IS_DEAD;
+					if (board[i][j] == WAS_DEAD_NOW_ALIVE) board[i][j] = IS_ALIVE;
+				}
+			}
+		}
+
+		private int updatedStatus(int state, int liveCount, int deadCount) {
+			if (state == IS_ALIVE && liveCount < 2) return WAS_ALIVE_NOW_DEAD;
+			if (state == IS_ALIVE && (liveCount == 2 || liveCount == 3)) return state;
+			if (state == IS_ALIVE && liveCount > 3) return WAS_ALIVE_NOW_DEAD;
+			if (state == IS_DEAD && liveCount == 3) return WAS_DEAD_NOW_ALIVE;
+			return state;
+		}
+
+
+		private int countNeighbors(int i, int j, int[][] board, int state) {
+			int ctr = 0;
+			if (i - 1 >= 0 && j - 1 >= 0 && getExistingState(board[i - 1][j - 1]) == state) {
+				ctr++;
+			} 
+			if (i - 1 >= 0 && getExistingState(board[i - 1][j]) == state) {
+				ctr++;
+			} 
+			if (j - 1 >= 0 && getExistingState(board[i][j - 1]) == state) {
+				ctr++;
+			} 
+			if (i + 1 < board.length && j + 1 < board[0].length && getExistingState(board[i + 1][j + 1]) == state) {
+				ctr++;
+			} 
+			if (i + 1 < board.length && getExistingState(board[i + 1][j]) == state) {
+				ctr++;
+			} 
+			if (j + 1 < board[0].length && getExistingState(board[i][j + 1]) == state) {
+				ctr++;
+			} 
+			if (i - 1 >= 0 && j + 1 < board[0].length && getExistingState(board[i - 1][j + 1]) == state) {
+				ctr++;
+			} 
+			if (i + 1 < board.length  && j - 1 >= 0 && getExistingState(board[i + 1][j - 1]) == state) {
+				ctr++;
+			} 
+			return ctr;
+		}
+
+		private int getExistingState(int value) {
+			if (value == IS_ALIVE || value == WAS_ALIVE_NOW_DEAD) return IS_ALIVE;
+			else return IS_DEAD;
+		}
+
+	}
+}
---
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/MaximumProductSubarray.java
@@ -0,0 +1,46 @@
+package com.partha.problemSolving.leetcode;
+
+/**
+ * https://leetcode.com/problems/maximum-product-subarray/
+ * @author partha
+ *
+ */
+public class MaximumProductSubarray {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	
+	
+	private static class  Solution2 {
+	    public int maxProduct(int[] nums) {
+	        int mpp=nums[0]; // mpp: maximum Positive Product
+	         int mnp=nums[0]; // mnp: Minimum Negative Product
+	         int omax=nums[0]; // overall maximum Product
+	         
+	         
+	         for(int i=1;i<nums.length;i++){
+	             
+	             int val=nums[i];
+	             if(val<0){
+	                 int temp1=mnp;
+	                 int temp2=mpp;
+	               mpp=Math.max(val,temp1*val);
+	               mnp=Math.min(val,temp2*val);    
+	             }else{
+	                mpp=Math.max(val,val*mpp);
+	                mnp=Math.min(val,val*mnp); 
+	             }
+	             omax=Math.max(mpp,omax);
+	             
+	         }
+	         
+	         return omax;
+	     }
+	 }
+	
+	
+
+}
