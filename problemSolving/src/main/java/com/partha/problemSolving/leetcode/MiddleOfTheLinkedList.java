7a7eabe3b84c2c7e5771bfef00e44d0f59206a1e
Partha Biswas <biswas.partha1@gmail.com>
2021-05-18 20:59:38 +0530

    adding some solution

---
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/MiddleOfTheLinkedList.java
@@ -0,0 +1,35 @@
+package com.partha.problemSolving.leetcode;
+
+/**
+ * https://leetcode.com/problems/middle-of-the-linked-list/
+ * @author partha
+ *
+ */
+public class MiddleOfTheLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private static class Solution {
+	    public ListNode middleNode(ListNode head) {
+	        ListNode slow = head;
+	        ListNode fast = head;
+	        while(fast!=null && fast.next!=null){
+	            slow = slow.next;
+	            fast = fast.next.next;
+	        }
+	        return slow;
+	    }
+	}
+	
+	private static class ListNode {
+	    int val;
+	    ListNode next;
+	    ListNode() {}
+	    ListNode(int val) { this.val = val; }
+	    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+
+}
