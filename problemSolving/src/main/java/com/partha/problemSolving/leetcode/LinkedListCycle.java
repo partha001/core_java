cb48367d87f2214b929b7ef51334dd3a67e68b49
Partha Biswas <biswas.partha1@gmail.com>
2021-05-24 03:25:19 +0530

    adding more solutions

---
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/LinkedListCycle.java
@@ -0,0 +1,68 @@
+package com.partha.problemSolving.leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * https://leetcode.com/problems/linked-list-cycle/
+ * @author partha
+ *
+ */
+public class LinkedListCycle {
+
+	public static void main(String[] args) {
+
+	}
+	
+	private static class Solution1 {
+	    
+	     boolean hasCycle(ListNode head) {
+	      Set<ListNode> nodesSeen = new HashSet<>();
+			    while (head != null) {
+			        if (nodesSeen.contains(head)) {
+			            return true;
+			        } else {
+			            nodesSeen.add(head);
+			        }
+			        head = head.next;
+			    }
+			    return false;
+	    }
+	}
+	
+	
+	//takes less memory and less execution time than the previous solution
+	private static class Solution2 {
+	     boolean hasCycle(ListNode node) {
+	      
+			  if(node==null)
+				return false;
+	        
+				boolean flag = false;
+				ListNode slowPointer = node;
+				ListNode fastPointer = node.next; 
+				while(fastPointer!=null && 
+						slowPointer!=null && 
+						fastPointer.next!=null // this condition avoid null pointer exception
+						){
+					fastPointer = fastPointer.next.next;
+					slowPointer = slowPointer.next;	
+					if(slowPointer==fastPointer){
+						flag = true;
+						break;
+					}
+				}
+				return flag;	        
+	    }
+	}
+	
+	class ListNode {
+	    int val;
+	    ListNode next;
+	    ListNode(int x) {
+	        val = x;
+	        next = null;
+	    }
+	}
+
+}
