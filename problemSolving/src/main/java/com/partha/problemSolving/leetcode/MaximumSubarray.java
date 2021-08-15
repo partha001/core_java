c3219c291d891b1d248839df9206617912bc3a21
Partha Biswas <biswas.partha1@gmail.com>
2021-08-15 08:41:45 +0530

    maximum subarray

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/MaximumSubarray.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/MaximumSubarray.java
@@ -1,13 +1,30 @@
 package com.partha.problemSolving.leetcode;
 
+/**
+ * https://leetcode.com/problems/maximum-subarray/
+ * @author partha
+ *
+ */
 public class MaximumSubarray {
 
 	public static void main(String[] args) {
 
 	}
 	
-	static class Solution1 {
-		
+	private static class Solution1 {
+	    public int maxSubArray(int[] nums) {
+	        if(nums.length==0)
+	            return 0;
+	        
+	        int[] dp = new int[nums.length];
+	        dp[0] = nums[0];
+	        int result = dp[0];
+	        for(int i=1;i<nums.length;i++){
+	            dp[i] = Math.max( nums[i] , dp[i-1]+ nums[i]);
+	            result = Math.max(result, dp[i]);
+	        }
+	        return result;
+	    }
 	}
 
 }
