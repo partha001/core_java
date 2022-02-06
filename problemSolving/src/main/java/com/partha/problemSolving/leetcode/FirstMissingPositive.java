1d8c95e83991feaddbe41d46fe59bb7d57edee26
Partha Biswas <biswas.partha1@gmail.com>
2022-02-06 21:00:28 +0530

    adding a better solution

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/FirstMissingPositive.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/FirstMissingPositive.java
@@ -15,6 +15,12 @@ public class FirstMissingPositive {
 
 	}
 	
+	/**
+	 * timeComplexity : O(n) 
+	 * spaceComplexity: n
+	 * @author partha
+	 *
+	 */
 	private static class Solution {
 	    public int firstMissingPositive(int[] nums) {
 	       Set<Integer> set = new HashSet();
@@ -36,5 +42,43 @@ public class FirstMissingPositive {
 	        return result;
 	    }
 	}
+	
+	/**
+	 * this is a better solution since its a constant space solution
+	 * timeComplexity : O(n) 
+	 * spaceComplexity: 1
+	 * @author partha
+	 *
+	 */
+	private static class Solution2 {
+		static int firstMissingPositive(int[] arr){
+		        int i=0;
+		        while(i<arr.length){
+		            // correct is the index where the ith element should actually be
+		            int correct = arr[i] - 1;
+		            if(arr[i]>0 && arr[i]<=arr.length && arr[i]!=arr[correct]){ /* First condition: Ignoring the negatives Second condition: If element=length of the array, just ignore it and move ahead */
+		                swap(arr,i,correct); // swap if not at the correct position
+		            }else{
+		                i++; // else move ahead
+		            }
+		        }
+
+		        // search for first missing number
+		        for (int index = 0; index < arr.length; index++) {
+		            if(arr[index]!=index + 1){
+		                return index + 1;
+		            }
+		        }
+
+		        // case 2
+		        return arr.length + 1;
+		    }
+
+		    static void swap(int[] arr, int first, int second){
+		        int temp=arr[first];
+		        arr[first]=arr[second];
+		        arr[second]=temp;
+		    }
+		}
 
 }
