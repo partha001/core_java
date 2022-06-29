1f08b5235686a1419e5b8f804301efa254b45f33
Partha Biswas <biswas.partha1@gmail.com>
2022-06-29 19:29:49 +0530

    code refactoring

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/CombinationSum.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/CombinationSum.java
@@ -14,38 +14,32 @@ public class CombinationSum {
 		List<List<Integer>> combinationSum = new Solution1().combinationSum(new int[] {7,2,3,6}, 7);
 		System.out.println("");
 	}
-	
+
 	static class Solution1 {
-	    List<List<Integer>> listOfCombinations = new ArrayList<>();
-	    
-	    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-	        List<Integer> combination = new ArrayList<Integer>();
-	        int sum = 0;
-	        generateCombinations(candidates, target, combination, sum, 0);
-	        return listOfCombinations;
-	    }
-	    
-	    private void generateCombinations(int[] candidates, int target, List<Integer> combination, int sum, int startIndex){
-	        if(sum == target){
-	            List<Integer> validCombination = new ArrayList<Integer>(combination);
-	            listOfCombinations.add(validCombination);
-	            return;
-	        }
-	        
-	        if(sum > target){
-	            return;
-	        }
-	        
-	        if(sum < target){
-	            for(int i = startIndex; i < candidates.length; i++){
-	                sum = sum + candidates[i];
-	                combination.add(candidates[i]);
-	                generateCombinations(candidates, target, combination, sum, i);
-	                sum = sum - candidates[i];
-	                combination.remove(combination.size() - 1);
-	            }
-	        }
-	    }
+		List<List<Integer>> listOfCombinations = new ArrayList<>();
+
+		public List<List<Integer>> combinationSum(int[] candidates, int target) {
+			List<Integer> combination = new ArrayList<Integer>();
+			backtrack(candidates, target, combination, 0, 0);
+			return listOfCombinations;
+		}
+
+		private void backtrack(int[] candidates,int target,List<Integer> combination,int sum,int start){
+			if(sum == target){
+				List<Integer> validCombination = new ArrayList<Integer>(combination);
+				listOfCombinations.add(validCombination);
+				return;
+			}
+
+			for(int i = start; i < candidates.length; i++){
+				if(sum + candidates[i]<=target){
+					combination.add(candidates[i]);
+					backtrack(candidates, target, combination, sum + candidates[i], i);
+					combination.remove(combination.size() - 1);
+				}
+
+			}
+		}
 	}
 
 }
