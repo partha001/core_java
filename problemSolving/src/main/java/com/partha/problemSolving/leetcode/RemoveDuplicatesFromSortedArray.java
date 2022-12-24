763ad8c1a551d04daeed2159b3f6623b2d5bb849
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-24 13:29:55 +0530

    remove duplicates

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/RemoveDuplicatesFromSortedArray.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -12,5 +12,19 @@ public class RemoveDuplicatesFromSortedArray {
 	}
 	
 	
+	class Solution {
+	    public int removeDuplicates(int[] nums) {
+	    	if(nums.length<=1) return nums.length; //edge case
+	    	
+	        int latest = 0;
+	        for(int i=1;i<nums.length;i++){
+	            if(nums[i]== nums[latest])
+	                continue;
+	            nums[++latest] = nums[i];
+	        }
+	        return latest+1;
+	    }
+	}
+	
 
 }
