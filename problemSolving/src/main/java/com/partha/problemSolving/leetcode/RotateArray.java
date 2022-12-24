5e29e572a8719b8fe7ef68ee739c21c37bb9e899
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-24 13:06:13 +0530

    formatting the code

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/RotateArray.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/RotateArray.java
@@ -12,8 +12,8 @@ public class RotateArray {
 	public static void main(String[] args) {
 		new Solution1().rotate(new int[] {10,11,12},1);
 	}
-	
-	
+
+
 	/**
 	 * @algorithm first picking up d elements from front to a temporary array<br>
 	 * then shifting the remaining elements in the array.<br>
@@ -24,34 +24,34 @@ public class RotateArray {
 	public static void rotateArray(int[] arr, int d){
 		int[] temparr = new int[d];
 		IntStream.range(0, d).forEach(i -> temparr[i]=arr[i]);
-		
+
 		for(int i= d ; i <arr.length; i++){
 			arr[i-d]=arr[i];
 		}
-		
+
 		int c=0;
 		for(int i=arr.length-d;i<arr.length;i++){
 			arr[i]=temparr[c++];
 		}
-		
+
 		System.out.println("hi");
 	}
 
-	
-	
+
+
 	//this is the more elegant solution
 	static class Solution1 {
-		
+
 		public void rotate(int[] nums, int k) {
 			int[] a = new int[nums.length];
-		    for (int i = 0; i < nums.length; i++) {
-		      a[(i + k) % nums.length] = nums[i];
-		    }
-		    for (int i = 0; i < nums.length; i++) {
-		      nums[i] = a[i];
-		    }
-		    
-		    IntStream.range(0, nums.length).forEach(i -> System.out.println(nums[i]));
+			for (int i = 0; i < nums.length; i++) {
+				a[(i + k) % nums.length] = nums[i];
+			}
+			for (int i = 0; i < nums.length; i++) {
+				nums[i] = a[i];
+			}
+
+			IntStream.range(0, nums.length).forEach(i -> System.out.println(nums[i]));
 		}
 	}
 
