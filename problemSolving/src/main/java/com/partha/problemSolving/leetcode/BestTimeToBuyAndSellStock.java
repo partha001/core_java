ab68e40626af4efaac42395add05ff183c8c2c0f
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-24 18:22:36 +0530

    code refactoring

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/BestTimeToBuyAndSellStock.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/BestTimeToBuyAndSellStock.java
@@ -9,11 +9,11 @@ public class BestTimeToBuyAndSellStock {
 
 	public static void main(String[] args) {
 		System.out.println(new Solution3().maxProfit(new int[] {7,1,5,3,6,4}));
-		
+
 		//new Solution2().maxProfit(new int[] {});
 
 	}
-	
+
 
 
 	/**
@@ -23,86 +23,83 @@ public class BestTimeToBuyAndSellStock {
 	 *
 	 */
 	static class Solution1 {
-		 public int maxProfit(int[] prices) {
-			 int maxProfit = 0;
-			 for(int i=1;i<prices.length;i++) {
-				 for(int j=0;j<i;j++) {
-					 if(prices[i]-prices[j]>maxProfit) {
-						 maxProfit = prices[i]-prices[j];
-					 }
-				 }
-			 }
-			 return maxProfit;
-		 }
+		public int maxProfit(int[] prices) {
+			int maxProfit = 0;
+			for(int i=1;i<prices.length;i++) {
+				for(int j=0;j<i;j++) {
+					if(prices[i]-prices[j]>maxProfit) {
+						maxProfit = prices[i]-prices[j];
+					}
+				}
+			}
+			return maxProfit;
+		}
 	}
-	
-	
+
+
 	/**
 	 * brining down be time complexity to O(n) by momoisation by taking two arrays
 	 */
 	static class Solution2 {
-		
-		 public int maxProfit(int[] prices) {
-			 
-			 //corner cases
-			 if(prices.length<=1) {
-				 return 0;
-			 }
-			 
-			 //this is to store the min till i-th element
-			 int minarr[] = new int[prices.length];
-			 minarr[0]=prices[0];
-			 for(int i=1;i<prices.length;i++) {
-				 minarr[i] = Math.min(prices[i], minarr[i-1]);
-			 }
-			 
-			 int[] profitArr = new int[prices.length];
-			 profitArr[0]=0; //since item needs to be bought first before selling
-			 for(int i=1;i<profitArr.length;i++) {
-				 profitArr[i] = prices[i]-minarr[i-1];
-			 }
-			 
-			 
-			 int maxProfit=profitArr[0];
-			 for(int i=1;i<profitArr.length;i++) {
-				 if(maxProfit<profitArr[i]) {
-					 maxProfit = profitArr[i];
-				 }
-			 }
-			 return maxProfit;		 
-			 
-		 }
+
+		public int maxProfit(int[] prices) {
+
+			//corner cases
+			if(prices.length<=1) {
+				return 0;
+			}
+
+			//this is to store the min till i-th element
+			int minarr[] = new int[prices.length];
+			minarr[0]=prices[0];
+			for(int i=1;i<prices.length;i++) {
+				minarr[i] = Math.min(prices[i], minarr[i-1]);
+			}
+
+			int[] profitArr = new int[prices.length];
+			profitArr[0]=0; //since item needs to be bought first before selling
+			for(int i=1;i<profitArr.length;i++) {
+				profitArr[i] = prices[i]-minarr[i-1];
+			}
+
+
+			int maxProfit=profitArr[0];
+			for(int i=1;i<profitArr.length;i++) {
+				if(maxProfit<profitArr[i]) {
+					maxProfit = profitArr[i];
+				}
+			}
+			return maxProfit;		 
+
+		}
 	}
-	
-	
+
+
 	/**
 	 * furthur reducing memory complexity by reducing above two arrays using 2 variables. 
 	 * thus am not storing the max profit at every element level 
 	 * @author biswaspa
 	 *
 	 */
-	static class Solution3{
-		
-		 public int maxProfit(int[] prices) {
-			 
-			 if(prices.length<=1)
-		            return 0;
-		        int maxProfit=0;
-		        int minPrice=prices[0];
-		        for(int i=1;i<prices.length;i++){
-		           if(prices[i]>=minPrice)
-		               maxProfit = Math.max(maxProfit,prices[i]-minPrice);
-		            else{
-		                minPrice = prices[i];
-		            }
-		        }
-		        return maxProfit;
-		 
-		 }
-		
+	private static class Solution3 {
+		public int maxProfit(int[] prices) {
+			if(prices.length<=1)
+				return 0;
+			int min = prices[0];
+			int maxProfit =0;
+			for(int i=1;i<prices.length;i++){
+				if(maxProfit<prices[i]-min){
+					maxProfit=prices[i] -min;
+				}
+				min = Math.min(min,prices[i]);
+			}
+			return maxProfit;
+		}
 	}
-	
-	
-		
-	
+
 }
+
+
+
+
+
