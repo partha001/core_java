f10c51b633152befd0035ebebbb7ad67b64c1e5e
partha <biswaspa@vmware.com>
2022-12-25 12:48:06 +0530

    jump game

--- problemSolving/src/main/java/com/partha/problemSolving/leetcode/JumpGame.java
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/JumpGame.java
@@ -11,7 +11,7 @@ public class JumpGame {
 
 	}
 	
-	class Solution {
+	private static class Solution1 {
 	    public boolean canJump(int[] nums) {
 	        int reachable = 0;
 	        for(int i=0; i<nums.length; i++){
@@ -22,6 +22,27 @@ public class JumpGame {
             return true;
 	    }
 	}
+	
+	
+	/*
+	 * 
+	 * https://www.youtube.com/watch?v=Yan0cv2cLy8
+	 * traversing in backward direction. practice this
+	 */
+	private static class Solution2 {
+	    public boolean canJump(int[] nums) {
+	        int n = nums.length;
+	        int lastTrue = n-1;
+	        for(int i = n-2;i>=0;i--){
+	            int jumpSize = nums[i];
+	            if(i+jumpSize >= n-1 || i+jumpSize>=lastTrue){ //can you jump from here and reach the end or atleast reach the point that can help reach the end?
+	                lastTrue = i;
+	            }
+	        }
+	        return lastTrue == 0;
+	    }
+	}
+	
 
 }
 
