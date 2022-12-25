9738d290e5540603c679839710d4d0b60b4d43e2
partha <biswaspa@vmware.com>
2022-12-25 17:06:16 +0530

    hands of straights

---
+++ problemSolving/src/main/java/com/partha/problemSolving/leetcode/HandsOfStraight.java
@@ -0,0 +1,22 @@
+package com.partha.problemSolving.leetcode;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
+/**
+ * https://leetcode.com/problems/hand-of-straights/
+ * @author biswaspa
+ *
+ */
+public class HandsOfStraight {
+
+	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+//		minHeap.re
+
+	}
+	
+	//https://www.youtube.com/watch?v=amnrMCVd2YI
+
+}
