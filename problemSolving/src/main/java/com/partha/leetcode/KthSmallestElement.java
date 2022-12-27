9ef9e0dfa6c3411431019c07ca0cf51474512f77
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-27 15:55:56 +0530

    adding official solution

--- problemSolving/src/main/java/com/partha/leetcode/KthSmallestElement.java
+++ problemSolving/src/main/java/com/partha/leetcode/KthSmallestElement.java
@@ -11,7 +11,12 @@ import java.util.List;
 public class KthSmallestElement {
 
 	public static void main(String[] args) {
-
+		TreeNode root = new TreeNode(3);
+		root.left = new TreeNode(1);
+		root.left.right = new TreeNode(2);
+		root.right = new TreeNode(4);
+		int result = new KthSmallestElement.Solution2().kthSmallest(root, 1);
+		System.out.println(result);
 	}
 	
 	//using inOrderTraversal
@@ -34,6 +39,54 @@ public class KthSmallestElement {
 	        }
 	    }
 	}
+	
+	
+	 private static class Solution2 {
+
+	     int count=0;
+	     TreeNode result=null;
+
+	     public int kthSmallest(TreeNode root, int k) {
+	         if(root==null)
+	             return -1;
+	         find(root,k);
+	         return result.val;
+	     }
+
+	     public void find(TreeNode node,int k){
+	       if(node!=null && count<k){
+	           if(node.left!= null) find(node.left,k);
+	           count++;
+	           if(count==k){
+	               this.result = node;
+	               return;
+	           }
+	             
+	           if(node.right!=null) find(node.right,k);
+	       }   
+	     }
+	 }
+	 
+	 
+	 /**
+	  * this is the officical solution
+	  * @author partha
+	  *
+	  */
+	 private static class Solution3 {
+		  public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> arr) {
+		    if (root == null) return arr;
+		    inorder(root.left, arr);
+		    arr.add(root.val);
+		    inorder(root.right, arr);
+		    return arr;
+		  }
+
+		  public int kthSmallest(TreeNode root, int k) {
+		    ArrayList<Integer> nums = inorder(root, new ArrayList<Integer>());
+		    return nums.get(k - 1);
+		  }
+		}
 	
 	
 	//helper class
