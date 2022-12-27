0446eb574bc3ff8be7b852b9a1295d8c2d1a1742
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-27 18:10:15 +0530

    adding comment

--- problemSolving/src/main/java/com/partha/leetcode/SameTree.java
+++ problemSolving/src/main/java/com/partha/leetcode/SameTree.java
@@ -19,7 +19,7 @@ public class SameTree {
 	    public boolean isSameTree(TreeNode p, TreeNode q) {
 	        if(p==null && q==null)
 	            return true;
-	        if(p==null  || q==null)
+	        if(p==null  || q==null) //i.e. if any is not null while the other is null
 	            return false;
 	        if(p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right, q.right))
 	            return true;
