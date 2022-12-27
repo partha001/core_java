a98ee10da9cdfc3f9bf51a00fd6ba27d1392ea3e
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-27 17:21:20 +0530

    code refactoring

--- problemSolving/src/main/java/com/partha/leetcode/InvertBinaryTree.java
+++ problemSolving/src/main/java/com/partha/leetcode/InvertBinaryTree.java
@@ -14,10 +14,14 @@ public class InvertBinaryTree {
 	//recursive solution
 	private static class Solution1 {
 		public TreeNode invertTree(TreeNode root) {
-			if(root == null) return null;
-			TreeNode tmp = root.left;
-			root.left = invertTree(root.right);
-			root.right = invertTree(tmp);
+			if(null == root)
+				return null;		
+
+			TreeNode left = invertTree(root.left);
+			TreeNode right = invertTree(root.right);
+			root.left = right;
+			root.right = left;
+
 			return root;
 		}
 	}
