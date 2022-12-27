9e526fb5f4e5dac7b4caad17ae5b30592605252d
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-27 17:57:14 +0530

    sameTree

---
+++ problemSolving/src/main/java/com/partha/leetcode/SameTree.java
@@ -0,0 +1,45 @@
+package com.partha.leetcode;
+
+import com.partha.leetcode.BalancedBinaryTree.TreeNode;
+
+/**
+ * https://leetcode.com/problems/same-tree/
+ * @author partha
+ *
+ */
+public class SameTree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	private static class Solution1 {
+	    public boolean isSameTree(TreeNode p, TreeNode q) {
+	        if(p==null && q==null)
+	            return true;
+	        if((p==null && q!=null) || (q==null && p!=null))
+	            return false;
+	        if(p.val==q.val && isSameTree(p.left,q.left) && isSameTree(p.right, q.right))
+	            return true;
+	        return false;
+	    }
+
+	}
+	
+	
+	private static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+		TreeNode() {}
+		TreeNode(int val) { this.val = val; }
+		TreeNode(int val, TreeNode left, TreeNode right) {
+			this.val = val;
+			this.left = left;
+			this.right = right;
+		}
+	}
+
+}
