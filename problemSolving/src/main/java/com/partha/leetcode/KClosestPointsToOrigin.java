4d113495fbae24be26e38dab482f31eb1c8eed50
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-28 21:26:59 +0530

    adding more code

--- problemSolving/src/main/java/com/partha/leetcode/KClosestPointsToOrigin.java
+++ problemSolving/src/main/java/com/partha/leetcode/KClosestPointsToOrigin.java
@@ -9,11 +9,38 @@ public class KClosestPointsToOrigin {
 
 	public static void main(String[] args) {
 		int[][] points = new int[][] {{5,-1},{3,3},{-2,4}};
-		new KClosestPointsToOrigin.Solution().kClosest(points, 2);
+		new KClosestPointsToOrigin.Solution1().kClosest(points, 2);
 	}
 	
 	
-	private static class Solution {
+	/**
+	 * my solution
+	 * @author partha
+	 *
+	 */
+	private static class Solution1 {
+	     public int[][] kClosest(int[][] points, int K) {
+		        PriorityQueue<int[]> pq = new PriorityQueue<>((x, y) -> x[0]-y[0]); // here storing int[distace, index] in the queue
+		        
+		        for(int i=0;i<points.length;i++){
+	                int[] point = points[i];
+	                int distance = point[0]*point[0] + point[1]*point[1];
+	                int[] element = new int[]{distance,i};
+	                pq.add(element);
+		        }
+		        
+		        int[][] result = new int[K][2];
+	            int i=0;
+	            while(i<K && !pq.isEmpty()){
+	               int[] head = pq.poll();
+	               result[i++] = points[head[1]];
+	            }
+	            return result;
+		    }
+	}
+	
+	
+	private static class Solution2 {
 	    // Priority Queue
 	    // TC: O(N Log N)
 	    // SC: O(K)
