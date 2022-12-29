faffed82ae1c5f2d6aad80d6af51000530a81222
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-29 08:27:34 +0530

    code

--- problemSolving/src/main/java/com/partha/leetcode/AddTwoNumbers.java
+++ problemSolving/src/main/java/com/partha/leetcode/AddTwoNumbers.java
@@ -28,6 +28,31 @@ public class AddTwoNumbers {
 		ListNode(int val) { this.val = val; }
 		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 	}
+	
+	
+	//practice this solution
+	private static class Solution2 {
+		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+			int carry = 0;
+			ListNode p, dummy = new ListNode(0);
+			p = dummy;
+			while (l1 != null || l2 != null || carry != 0) {
+				if (l1 != null) {
+					carry += l1.val;
+					l1 = l1.next;
+				}
+				if (l2 != null) {
+					carry += l2.val;
+					l2 = l2.next;
+				}
+				p.next = new ListNode(carry%10);
+				carry /= 10;
+				p = p.next;
+			}
+			return dummy.next;
+		}
+	}
+	
 
 	static class Solution {	
 
@@ -86,26 +111,6 @@ public class AddTwoNumbers {
 	}
 
 
-	private static class Solution2 {
-		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-			int carry = 0;
-			ListNode p, dummy = new ListNode(0);
-			p = dummy;
-			while (l1 != null || l2 != null || carry != 0) {
-				if (l1 != null) {
-					carry += l1.val;
-					l1 = l1.next;
-				}
-				if (l2 != null) {
-					carry += l2.val;
-					l2 = l2.next;
-				}
-				p.next = new ListNode(carry%10);
-				carry /= 10;
-				p = p.next;
-			}
-			return dummy.next;
-		}
-	}
+	
 
 }
