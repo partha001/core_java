9418d3a7e69c8f95e9bec5c91546f169fdb2bcb7
partha-desktop\partha <biswas.partha1@gmail.com>
2022-12-29 09:00:25 +0530

    code refactoring

--- problemSolving/src/main/java/com/partha/leetcode/LinkedListCycle.java
+++ problemSolving/src/main/java/com/partha/leetcode/LinkedListCycle.java
@@ -13,56 +13,55 @@ public class LinkedListCycle {
 	public static void main(String[] args) {
 
 	}
-	
-	private static class Solution1 {
-	    
-	     boolean hasCycle(ListNode head) {
-	      Set<ListNode> nodesSeen = new HashSet<>();
-			    while (head != null) {
-			        if (nodesSeen.contains(head)) {
-			            return true;
-			        } else {
-			            nodesSeen.add(head);
-			        }
-			        head = head.next;
-			    }
-			    return false;
-	    }
-	}
-	
-	
+
+
 	//takes less memory and less execution time than the previous solution
+	// practice this solution
 	private static class Solution2 {
-	     boolean hasCycle(ListNode node) {
-	      
-			  if(node==null)
-				return false;
-	        
-				boolean flag = false;
-				ListNode slowPointer = node;
-				ListNode fastPointer = node.next; 
-				while(fastPointer!=null && 
-						slowPointer!=null && 
-						fastPointer.next!=null // this condition avoid null pointer exception
-						){
-					fastPointer = fastPointer.next.next;
-					slowPointer = slowPointer.next;	
-					if(slowPointer==fastPointer){
-						flag = true;
-						break;
-					}
-				}
-				return flag;	        
-	    }
+		
+		public boolean hasCycle(ListNode head) {
+			ListNode fast = head;
+			ListNode slow = head;
+			while(fast!=null && fast.next!=null){
+				fast = fast.next.next;
+				slow = slow.next;
+
+				if(fast==slow)
+					return true;
+			}
+			return false;
+		}
 	}
-	
+
+
+
+
+	private static class Solution1 {
+
+		boolean hasCycle(ListNode head) {
+			Set<ListNode> nodesSeen = new HashSet<>();
+			while (head != null) {
+				if (nodesSeen.contains(head)) {
+					return true;
+				} else {
+					nodesSeen.add(head);
+				}
+				head = head.next;
+			}
+			return false;
+		}
+	}
+
+
+
+
 	class ListNode {
-	    int val;
-	    ListNode next;
-	    ListNode(int x) {
-	        val = x;
-	        next = null;
-	    }
+		int val;
+		ListNode next;
+		ListNode(int x) {
+			val = x;
+			next = null;
+		}
 	}
 
 }
