fc17f9081e84b945851645e2cc16bdc086b35949
partha-desktop\partha <biswas.partha1@gmail.com>
2023-01-01 21:30:12 +0530

    graph valid tree

--- problemSolving/src/main/java/com/partha/leetcode/GraphValidTree.java
+++ problemSolving/src/main/java/com/partha/leetcode/GraphValidTree.java
@@ -3,9 +3,12 @@ package com.partha.leetcode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
+import java.util.Stack;
 import java.util.stream.IntStream;
 
 /**
@@ -19,15 +22,241 @@ public class GraphValidTree {
 		// TODO Auto-generated method stub
 
 	}
-	
-	
+
+
+	/**
+	 * approach1: Iterative DFS
+	 * @author partha
+	 * @source: leetcode premium
+	 */
+	private static class Solution1{
+		public boolean validTree(int n, int[][] edges) {
+
+			List<List<Integer>> adjacencyList = new ArrayList<>();
+			for (int i = 0; i < n; i++) {
+				adjacencyList.add(new ArrayList<>());
+			}
+			for (int[] edge : edges) {
+				adjacencyList.get(edge[0]).add(edge[1]);
+				adjacencyList.get(edge[1]).add(edge[0]);
+			}
+
+			Map<Integer, Integer> parent = new HashMap<>();
+			parent.put(0, -1);
+			Stack<Integer> stack = new Stack<>();
+			stack.push(0);
+
+			while (!stack.isEmpty()) {
+				int node = stack.pop();
+				for (int neighbour : adjacencyList.get(node)) {
+					if (parent.get(node) == neighbour) {
+						continue;
+					}
+					if (parent.containsKey(neighbour)) {
+						return false;
+					}
+					stack.push(neighbour);
+					parent.put(neighbour, node);
+				}
+			}
+
+			return parent.size() == n;   
+		}
+	}
+
+
+	/**
+	 * @approach2: recursive dfs
+	 * @author partha
+	 * @source: leetcode official solution
+	 * @link : https://leetcode.com/problems/graph-valid-tree/solutions/539585/graph-valid-tree/
+	 */
+	private static class Solution2 {
+
+		private List<List<Integer>> adjacencyList = new ArrayList<>();
+		private Set<Integer> seen = new HashSet<>();
+
+
+		public boolean validTree(int n, int[][] edges) {
+
+			if (edges.length != n - 1) return false;
+
+			for (int i = 0; i < n; i++) {
+				adjacencyList.add(new ArrayList<>());
+			}
+			for (int[] edge : edges) {
+				adjacencyList.get(edge[0]).add(edge[1]);
+				adjacencyList.get(edge[1]).add(edge[0]);
+			}
+
+			// We return true iff no cycles were detected,
+			// AND the entire graph has been reached.
+			return dfs(0, -1) && seen.size() == n;   
+		}
+
+		public boolean dfs(int node, int parent) {
+			if (seen.contains(node)) return false;
+			seen.add(node);
+			for (int neighbour : adjacencyList.get(node)) {
+				if (parent != neighbour) {
+					boolean result = dfs(neighbour, node);
+					if (!result) return false;
+				}
+			}
+			return true;
+		}
+	}
+
+
+
+	/**
+	 * approach: iterative bfs
+	 * @source : leetcode official solution
+	 * @link: https://leetcode.com/problems/graph-valid-tree/solutions/539585/graph-valid-tree/
+	 * @TimeComplexity: O(N) where n is the number of nodes
+	 * @spaceComplexity: O(N) 
+	 * 
+	 */
+	private static class Solution3 {
+		public boolean validTree(int n, int[][] edges) {
+
+			if (edges.length != n - 1) return false;
+
+			// Make the adjacency list.
+			List<List<Integer>> adjacencyList = new ArrayList<>();
+			for (int i = 0; i < n; i++) {
+				adjacencyList.add(new ArrayList<>());
+			}
+			for (int[] edge : edges) {
+				adjacencyList.get(edge[0]).add(edge[1]);
+				adjacencyList.get(edge[1]).add(edge[0]);
+			}
+
+			Queue<Integer> queue = new LinkedList<>();
+			Set<Integer> seen = new HashSet<>();
+			queue.offer(0);
+			seen.add(0);
+
+			while (!queue.isEmpty()) {
+				int node = queue.poll();
+				for (int neighbour : adjacencyList.get(node)) {
+					if (seen.contains(neighbour)) continue;
+					seen.add(neighbour);
+					queue.offer(neighbour);
+				}
+			}
+
+			return seen.size() == n;   
+		}
+
+	}
+
+
+	/**
+	 * @approach: using union and find algorithm
+	 * @author partha
+	 * @source: leetcode official solution page
+	 * @link : https://leetcode.com/problems/graph-valid-tree/solutions/539585/graph-valid-tree/	 
+	 * @timeComplexity: Time Complexity : O(N⋅α(N)) .  where α(N) is Inverse Ackerman Function 
+	 * @spaceComplexity: O(N).
+	 */
+	private static class Solution4 {
+
+		public boolean validTree(int n, int[][] edges) {
+
+			// Condition 1: The graph must contain n - 1 edges.
+			if (edges.length != n - 1) return false;
+
+			// Condition 2: The graph must contain a single connected component.
+			// Create a new UnionFind object with n nodes. 
+			UnionFind unionFind = new UnionFind(n);
+			// Add each edge. Check if a merge happened, because if it 
+			// didn't, there must be a cycle.
+			for (int[] edge : edges) {
+				int A = edge[0];
+				int B = edge[1];
+				if (!unionFind.union(A, B)) {
+					return false;
+				}
+			}
+
+			// If we got this far, there's no cycles!
+			return true;
+		}
+
+	}
+
+
+	private static class UnionFind {
+
+		private int[] parent;
+		private int[] size; // We use this to keep track of the size of each set.
+
+		// For efficiency, we aren't using makeset, but instead initialising
+		// all the sets at the same time in the constructor.
+		public UnionFind(int n) {
+			parent = new int[n];
+			size = new int[n];
+			for (int node = 0; node < n; node++) {
+				parent[node] = node;
+				size[node] = 1;
+			}
+		}
+
+		// The find method, with path compression. There are ways of implementing
+		// this elegantly with recursion, but the iterative version is easier for
+		// most people to understand!
+		public int find(int A) {
+			// Step 1: Find the root.
+			int root = A;
+			while (parent[root] != root) {
+				root = parent[root];
+			}
+			// Step 2: Do a second traversal, this time setting each node to point
+			// directly at A as we go.
+			while (A != root) {
+				int oldRoot = parent[A];
+				parent[A] = root;
+				A = oldRoot;
+			}
+			return root;
+		}
+
+		// The union method, with optimization union by size. It returns True if a
+		// merge happened, False if otherwise.
+		public boolean union(int A, int B) {
+			// Find the roots for A and B.
+			int rootA = find(A);
+			int rootB = find(B);
+			// Check if A and B are already in the same set.
+			if (rootA == rootB) {
+				return false;
+			}
+			// We want to ensure the larger set remains the root.
+			if (size[rootA] < size[rootB]) {
+				// Make rootB the overall root.
+				parent[rootA] = rootB;
+				// The size of the set rooted at B is the sum of the 2.
+				size[rootB] += size[rootA];
+			}
+			else {
+				// Make rootA the overall root.
+				parent[rootB] = rootA;
+				// The size of the set rooted at A is the sum of the 2.
+				size[rootA] += size[rootB];
+			}
+			return true;
+		} 
+	}
+
+
 	/**
 	 * https://www.youtube.com/watch?v=bXsUuownnoQ
 	 * @author partha
-	 * solution to be tested. since premium problem so not tested yet
+	 * failed when executed in leetcode . need to be looked into
 	 *
 	 */
-	public class Solution {
+	public class Solution5 {
 	    /**
 	     * @param n: An integer . denotes total node count
 	     * @param edges: a list of undirected edges
--- problemSolving/src/main/java/com/partha/leetcode/WallsAndGates.java
+++ problemSolving/src/main/java/com/partha/leetcode/WallsAndGates.java
@@ -1,7 +1,8 @@
 package com.partha.leetcode;
 
-import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 /**
@@ -17,12 +18,63 @@ public class WallsAndGates {
 	}
 	
 	
+	
+	/**
+	 * @bfs 
+	 * @author partha
+	 * @source : leetcode official solution
+	 * : https://leetcode.com/problems/walls-and-gates/solutions/127819/walls-and-gates/
+	 * @TC: O(MN)
+	 * @SC: O(MN)
+	 */
+	private static class Solution1{
+		private static final int EMPTY = Integer.MAX_VALUE;
+		private static final int GATE = 0;
+		private static final List<int[]> DIRECTIONS = Arrays.asList(
+		        new int[] { 1,  0},
+		        new int[] {-1,  0},
+		        new int[] { 0,  1},
+		        new int[] { 0, -1}
+		);
+
+
+
+		public void wallsAndGates(int[][] rooms) {
+		    int m = rooms.length;
+		    if (m == 0) return;
+		    int n = rooms[0].length;
+		    Queue<int[]> q = new LinkedList<>();
+		    for (int row = 0; row < m; row++) {
+		        for (int col = 0; col < n; col++) {
+		            if (rooms[row][col] == GATE) {
+		                q.add(new int[] { row, col });
+		            }
+		        }
+		    }
+		    while (!q.isEmpty()) {
+		        int[] point = q.poll();
+		        int row = point[0];
+		        int col = point[1];
+		        for (int[] direction : DIRECTIONS) {
+		            int r = row + direction[0];
+		            int c = col + direction[1];
+		            if (r < 0 || c < 0 || r >= m || c >= n || rooms[r][c] != EMPTY) {
+		                continue;
+		            }
+		            rooms[r][c] = rooms[row][col] + 1;
+		            q.add(new int[] { r, c });
+		        }
+		    }
+		}
+	}
+	
+	
 	/**
 	 * https://www.youtube.com/watch?v=e69C6xhiSQE
 	 * @author partha
 	 *
 	 */
-	private static class Solution1 {
+	private static class Solution2 {
 		       
 	    public static int[][] wallsAndGates(int[][] a, int n, int m) {
 	        int[][] directions = new int[][]{{0,-1},{-1,0},{0,1},{1,0}};
