df74bb52c220820932af4a831b95282b16525683
partha <partha@DESKTOP-529H4F1>
2023-01-04 04:45:49 +0530

    adding the approach of expanding around the center

--- problemSolving/src/main/java/com/partha/leetcode/LongestPalindromicSubstring.java
+++ problemSolving/src/main/java/com/partha/leetcode/LongestPalindromicSubstring.java
@@ -100,5 +100,32 @@ public class LongestPalindromicSubstring {
 			return s.substring(start,start + maxLength);
 		}
 	}
+	
+	
+	private static class Solution3 {
+		int start =0;
+		int maxLength=0;
+
+		    public String longestPalindrome(String s) {
+		        if (s == null || s.length() < 1) return "";
+		        for (int i = 0; i < s.length(); i++) {
+		            expandAroundCenter(s, i, i);
+		            expandAroundCenter(s, i, i + 1);
+		        }
+		        return s.substring(start, start+maxLength);
+		    }
+
+		    private void  expandAroundCenter(String s, int left, int right) {
+		        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+		            left--;
+		            right++;
+		        }
+		         if(maxLength<right-left-1)
+		        {
+		            start=left+1;
+		            maxLength=right-left-1;
+		        }
+		    }
+		}
 
 }
