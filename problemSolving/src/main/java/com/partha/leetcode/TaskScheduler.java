865302f938073bc08065752e1fcfdfdf5bf46003
partha biswas <biswas.partha1@gmail.com>
2023-01-05 06:49:57 +0530

    Update TaskScheduler.java

--- problemSolving/src/main/java/com/partha/leetcode/TaskScheduler.java
+++ problemSolving/src/main/java/com/partha/leetcode/TaskScheduler.java
@@ -41,7 +41,7 @@ public class TaskScheduler {
 	        int f_max = frequencies[25];
 	        int idle_time = (f_max - 1) * n;
 	        
-	        for (int i = frequencies.length - 2; i >= 0 && idle_time > 0; --i) {
+	        for (int i = frequencies.length - 2; i >= 0 && idle_time > 0; i--) {
 	            idle_time -= Math.min(f_max - 1, frequencies[i]); 
 	        }
 	        idle_time = Math.max(0, idle_time);
