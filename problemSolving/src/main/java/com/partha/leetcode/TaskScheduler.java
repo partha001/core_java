827c24e0df26b73f372c909a2d612604ecdf2a92
partha biswas <biswas.partha1@gmail.com>
2023-01-05 06:57:43 +0530

    Update TaskScheduler.java

--- problemSolving/src/main/java/com/partha/leetcode/TaskScheduler.java
+++ problemSolving/src/main/java/com/partha/leetcode/TaskScheduler.java
@@ -41,7 +41,7 @@ public class TaskScheduler {
 	        int f_max = frequencies[25];
 	        int idle_time = (f_max - 1) * n; //for k tasks there would be k-1 intervals . hence total interval (k-1) * n
 	        
-	        for (int i = frequencies.length - 2; i >= 0 && idle_time > 0; i--) { //node the boundary condition here
+	        for (int i = frequencies.length - 2; i >= 0 && idle_time > 0; i--) { //note the boundary condition here
 	            idle_time -= Math.min(f_max - 1, frequencies[i]); 
 	        }
 	        idle_time = Math.max(0, idle_time);
