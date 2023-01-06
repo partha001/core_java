0699392818b10fcf2f7154ddc0828cd19c42c2dc
partha-desktop\partha <biswas.partha1@gmail.com>
2023-01-06 07:40:42 +0530

    indenting

--- problemSolving/src/main/java/com/partha/leetcode/PalindromePartitioning.java
+++ problemSolving/src/main/java/com/partha/leetcode/PalindromePartitioning.java
@@ -19,37 +19,37 @@ public class PalindromePartitioning {
 
 	//using backtracking
 	private static class Solution {
-	    public List<List<String>> partition(String s) {
-				List<List<String>> result = new ArrayList<List<String>>();
-				dfs(0, result, new ArrayList<String>(), s);
-				return result;
+		public List<List<String>> partition(String s) {
+			List<List<String>> result = new ArrayList<List<String>>();
+			dfs(0, result, new ArrayList<String>(), s);
+			return result;
+		}
+
+		void dfs(int start, List<List<String>> result, List<String> currentList, String s) {
+			//ie. when the full length of string is covered then adding whatever is there in currentList to result
+			if (start >= s.length()){ 
+				result.add(new ArrayList<String>(currentList));
+				return;
 			}
 
-			void dfs(int start, List<List<String>> result, List<String> currentList, String s) {
-				//ie. when the full length of string is covered then adding whatever is there in currentList to result
-				if (start >= s.length()){ 
-					result.add(new ArrayList<String>(currentList));
-	                return;
-	            }
-				
-				
-				for (int end = start; end < s.length(); end++) {
-					if (isPalindrome(s, start, end)) {
-						// add current substring in the currentList
-						currentList.add(s.substring(start, end + 1));
-						dfs(end + 1, result, currentList, s);
-						// backtrack and remove the current substring from currentList
-						currentList.remove(currentList.size() - 1);
-					}
-				}
-			}
 
-			boolean isPalindrome(String s, int low, int high) {
-				while (low < high) {
-					if (s.charAt(low++) != s.charAt(high--)) return false;
+			for (int end = start; end < s.length(); end++) {
+				if (isPalindrome(s, start, end)) {
+					// add current substring in the currentList
+					currentList.add(s.substring(start, end + 1));
+					dfs(end + 1, result, currentList, s);
+					// backtrack and remove the current substring from currentList
+					currentList.remove(currentList.size() - 1);
 				}
-				return true;
 			}
+		}
+
+		boolean isPalindrome(String s, int low, int high) {
+			while (low < high) {
+				if (s.charAt(low++) != s.charAt(high--)) return false;
+			}
+			return true;
+		}
 	}
 
 	//backtracking + dp
