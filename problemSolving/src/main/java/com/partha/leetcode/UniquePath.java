5ad088907752f04b1f453a02f587c6bb7b0a5cbc
partha-desktop\partha <biswas.partha1@gmail.com>
2023-01-19 19:23:04 +0530

    adding comments

--- problemSolving/src/main/java/com/partha/leetcode/UniquePath.java
+++ problemSolving/src/main/java/com/partha/leetcode/UniquePath.java
@@ -44,7 +44,7 @@ public class UniquePath {
 	        int[][] dp = new int[m][n];
 	        for(int i=0;i<m;i++){
 	            for(int j=0;j<n;j++){
-	                if(i==0 || j==0){
+	                if(i==0 || j==0){ //takes care of populating the first row and first column
 	                    dp[i][j] = 1;
 	                }else {
 	                    dp[i][j]= dp[i][j-1] + dp[i-1][j];
