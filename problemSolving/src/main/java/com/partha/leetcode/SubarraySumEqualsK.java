63a5094901fd20e62c5a8a6f6753c7e0c1f74694
partha-desktop\partha <biswas.partha1@gmail.com>
2023-02-26 21:18:18 +0530

    SubarraySumEqualsK

---
+++ problemSolving/src/main/java/com/partha/leetcode/SubarraySumEqualsK.java
@@ -0,0 +1,40 @@
+package com.partha.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode.com/problems/subarray-sum-equals-k/
+ * @author partha
+ *
+ */
+public class SubarraySumEqualsK {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	/**
+	 * https://www.youtube.com/watch?v=fFVZt-6sgyo
+	 * @author partha
+	 *
+	 */
+	private static class Solution1 {
+	    public int subarraySum(int[] nums, int k) {
+	        int result =0;
+	        int currentSum =0;
+	        Map<Integer, Integer> map = new HashMap<Integer,Integer>(); //this map contains the prefixSum
+	        map.put(0,1);
+	        for(int i : nums){
+	            currentSum += i;
+	            int difference = currentSum -k;
+	            result += map.getOrDefault(difference,0);
+	            map.put(currentSum , map.getOrDefault(currentSum,0)+1);
+	        }
+	        return result;
+	    }
+	}
+
+}
