11411d48b695c8232b30e12cea7bced917edef1b
partha <biswaspa@vmware.com>
2023-02-28 14:09:07 +0530

    H index

---
+++ problemSolving/src/main/java/com/partha/leetcode/HIndex.java
@@ -0,0 +1,87 @@
+package com.partha.leetcode;
+
+import java.util.Arrays;
+
+public class HIndex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static class Solution1 {
+
+		public int hIndex(int[] citations) {
+			Arrays.sort(citations);
+			int n=citations.length;
+			int index=0;
+			while(index<n && citations[index]<n-index)
+			{
+				index++;
+			}
+			return n-index;
+		}
+
+	}
+
+	
+	/**
+	 * using bucketSort
+	 * @author biswaspa
+	 *
+	 */
+	private static class Solution2 {
+		public int hIndex(int[] citations) {
+		    int n = citations.length;
+		    int[] buckets = new int[n+1];
+		    for(int c : citations) {
+		        if(c >= n) {
+		            buckets[n]++;
+		        } else {
+		            buckets[c]++;
+		        }
+		    }
+		    int count = 0;
+		    for(int i = n; i >= 0; i--) {
+		        count += buckets[i];
+		        if(count >= i) {
+		            return i;
+		        }
+		    }
+		    return 0;
+		}
+	}
+	
+	
+	/**
+	 * using binary search algorithm
+	 * TC nLog n
+	 * @author biswaspa
+	 *
+	 */
+	private static class Solution3{
+		
+		public int hIndex(int[] citations) {
+		    int n = citations.length;
+		    int[] buckets = new int[n+1];
+		    for(int c : citations) {
+		        if(c >= n) {
+		            buckets[n]++;
+		        } else {
+		            buckets[c]++;
+		        }
+		    }
+		    int count = 0;
+		    for(int i = n; i >= 0; i--) {
+		        count += buckets[i];
+		        if(count >= i) {
+		            return i;
+		        }
+		    }
+		    return 0;
+		}
+	}
+	
+
+
+}
