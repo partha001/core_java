aa849c9350564affb14191549cb1e13874a6ac1a
partha <biswaspa@vmware.com>
2023-02-28 17:24:03 +0530

    code refactoring

--- problemSolving/src/main/java/com/partha/leetcode/StudentAttendance1.java
+++ problemSolving/src/main/java/com/partha/leetcode/StudentAttendance1.java
@@ -11,36 +11,37 @@ public class StudentAttendance1 {
 		// TODO Auto-generated method stub
 
 	}
-	
-	
+
+
 	private static class Solution {
-	    public boolean checkRecord(String s) {
-	        int absent=0;
-	        int i=0;
-	        while(i<s.length()){
-	            char current = s.charAt(i);
-	            if(current=='A'){
-	                absent++;
-	                if(absent>=2)
-	                    return false;
-	                i++;
-	                continue;
-	            }
-
-	            int lateCount=0;
-	            while(i<s.length() && s.charAt(i)=='L'){
-	                lateCount++;
-	                i++;
-	                if(lateCount>=3)
-	                    return false;
-	            }
-
-	            if(current=='P')
-	                i++;
-	                     
-	        }
-	        return true;
-	    }
+		public boolean checkRecord(String s) {
+			int absent=0;
+			int i=0;
+			while(i<s.length()){
+				char current = s.charAt(i);
+				if(current=='A'){ //absent case
+					absent++;
+					if(absent>=2)
+						return false;
+					i++;
+					continue;
+				}
+				else if(current=='P'){ //case present
+					i++;
+					continue;
+				}
+				else {	//case late
+					int lateCount=0;
+					while(i<s.length() && s.charAt(i)=='L'){
+						lateCount++;
+						i++;
+						if(lateCount>=3)
+							return false;
+					}
+				}
+			}
+			return true;
+		}
 	}
 
 }
