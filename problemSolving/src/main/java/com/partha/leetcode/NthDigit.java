2d252a087d406bcdd750907d07384c208c90dba6
partha-desktop\partha <biswas.partha1@gmail.com>
2023-03-05 12:23:52 +0530

    nth digit

---
+++ problemSolving/src/main/java/com/partha/leetcode/NthDigit.java
@@ -0,0 +1,45 @@
+package com.partha.leetcode;
+
+public class NthDigit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * TC O(logn)
+	 * SC O(1)
+	 * @author partha
+	 *
+	 */
+	private static class Solution {
+	    public int findNthDigit(int n) {
+	        int count = 1;
+	        long end = 9;
+	        long start = 1;
+	        boolean flag = false;
+	        while(!flag) {
+	            if((end - start + 1) * count > n) flag = true;
+	            else {
+	                n -= ((end - start + 1) * count);
+	                end = end*10 + 9;
+	                count++;
+	                start *= 10;
+	            }
+	        }
+	        int nextRem = n % count;
+	        long targetValue = start + n / count - 1;
+	        if(nextRem != 0) {
+	            targetValue++;
+	            nextRem = count - nextRem;
+	            while(nextRem != 0) {
+	                targetValue /= 10;
+	                nextRem--;
+	            } 
+	        }
+	        return (int)targetValue % 10;
+	    }
+	}
+	
+}
