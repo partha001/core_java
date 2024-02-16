9ff384dd81dad8a332ada0269db38759d52dceeb
partha biswas <biswas.partha1@gmail.com>
2024-02-16 17:41:10 +0530

    adding comment

--- problemSolving/src/main/java/com/partha/leetcode/NextGreaterElement2.java
+++ problemSolving/src/main/java/com/partha/leetcode/NextGreaterElement2.java
@@ -16,56 +16,61 @@ public class NextGreaterElement2 {
 		int[] result = new NextGreaterElement2.Solution2().nextGreaterElements(input);
 		System.out.println(result);
 	}
-	
-	
+
+
 	/**
-     * Time complexity:O(N)
-     * Space complexity:O(N)
+	 * Time complexity:O(N)
+	 * Space complexity:O(N)
 	 * @author partha
 	 */
-	private static class Solution1 {
-	    public int[] nextGreaterElements(int[] nums) {
-	        int n = nums.length;
-	        int nge[] = new int[n];
-	        Stack < Integer > st = new Stack < > ();
-	        for (int i = 2 * n - 1; i >= 0; i--) {
-	            while (st.isEmpty() == false && st.peek() <= nums[i % n]) {
-	                st.pop();
-	            }
-	            if (i < n) {
-	                if (st.isEmpty() == false) nge[i] = st.peek();
-	                else nge[i] = -1;
-	            }
-	            st.push(nums[i % n]);
-	        }
-	        return nge;
-	    }
+	private static class Solution {
+		public int[] nextGreaterElements(int[] nums) {
+			int[] result = new int[nums.length];
+			int n = nums.length;
+			Stack<Integer> stack = new Stack();
+			for(int i=2* nums.length-1;i>=0;i--){
+				int current = nums[i %n];
+
+				while(!stack.isEmpty() && stack.peek()<=current) //popping all elements lesser than current
+					stack.pop();
+
+				if(i<n){ //updating result array
+					if(!stack.isEmpty())
+						result[i] = stack.peek();
+					else
+						result[i] = -1;               
+				}
+
+				stack.push(current); //putting current element in the stack again
+			}
+			return result;
+		}
 	}
-	
+
 	/**
-     * Time complexity:O(N)
-     * Space complexity:O(N)
+	 * Time complexity:O(N)
+	 * Space complexity:O(N)
 	 * @author partha
 	 * https://www.youtube.com/watch?v=ARkl69eBzhY
 	 */
 	private static class Solution2 {
-    public int[] nextGreaterElements(int[] nums) {
-	        int len = nums.length;
-	        int[] result = new int[nums.length];
-	        Arrays.fill(result, -1);
-	        Stack<Integer> stack = new Stack<>(); //the stack will hold the index of the elements
-	        for (int i = 0; i < len*2; i++) {
-	            int index = i % len;
-	            while(!stack.isEmpty() && nums[stack.peek()] < nums[index]) {
-	                int pop = stack.pop();
-	                result[pop] = nums[index];
-	            }
+		public int[] nextGreaterElements(int[] nums) {
+			int len = nums.length;
+			int[] result = new int[nums.length];
+			Arrays.fill(result, -1);
+			Stack<Integer> stack = new Stack<>(); //the stack will hold the index of the elements
+			for (int i = 0; i < len*2; i++) {
+				int index = i % len;
+				while(!stack.isEmpty() && nums[stack.peek()] < nums[index]) {
+					int pop = stack.pop();
+					result[pop] = nums[index];
+				}
 
-                if(i<len) //this condition is optional but stack.push() has to be done
-	                 stack.push(index);
-	        }
-	        return result;
-	    }
-}
+				if(i<len) //this condition is optional but stack.push() has to be done
+					stack.push(index);
+			}
+			return result;
+		}
+	}
 
 }
