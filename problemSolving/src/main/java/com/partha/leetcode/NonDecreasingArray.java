85dec11224df574e6172d1fbe8996b071870f9fb
partha biswas <biswas.partha1@gmail.com>
2024-02-25 14:27:56 +0530

    improving code readability

--- problemSolving/src/main/java/com/partha/leetcode/NonDecreasingArray.java
+++ problemSolving/src/main/java/com/partha/leetcode/NonDecreasingArray.java
@@ -51,10 +51,13 @@ public class NonDecreasingArray {
 	private static class Solution2{
 		public boolean checkPossibility(int[] nums) {
 			for (int i=1, modified=0; i<nums.length; i++) {
-				if (nums[i-1] > nums[i]) {
-					if (modified++ == 1) return false;
-					if (i<2 || nums[i-2] <= nums[i]) nums[i-1] = nums[i];
-					else nums[i] = nums[i-1];
+				if (nums[i-1] > nums[i]) { //if violates increasing order
+					if (modified++ == 1) 	//if number of modification reaches threshold
+						return false;
+					if (i<2 || nums[i-2] <= nums[i]) //if its for first 2 element or 
+						nums[i-1] = nums[i];   //making the modification at i-1th index
+					else 
+						nums[i] = nums[i-1];   //making the modification at i-th index
 				}
 			}
 			return true;
