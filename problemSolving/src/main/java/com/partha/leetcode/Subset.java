8e45bcbcc0b66aef853907562472dd00facc511d
partha biswas <biswas.partha1@gmail.com>
2024-02-26 16:32:28 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/Subset.java
+++ problemSolving/src/main/java/com/partha/leetcode/Subset.java
@@ -13,29 +13,27 @@ public class Subset {
 	public static void main(String[] args) {
 
 	}
-	
+
 	static class Solution1 {
-	    public List<List<Integer>> subsets(int[] nums) {
-	        List<List<Integer>> result = new ArrayList<List<Integer>>();
-	        List<Integer> sublist = new ArrayList<Integer>();
-	        if(nums.length==0)
-	            return result ;
-	        backtrack(nums,result,sublist,0);
-	        return result;
-	    }
-	    
-	    public void backtrack(int[] nums,
-	                          List<List<Integer>> result,
-	                          List<Integer> sublist,
-	                          int start){
-	        List<Integer> validSubset = new ArrayList<Integer>(sublist); //here is the catch in this prob
-	        result.add(validSubset);        
-	        for(int i = start; i < nums.length; i++){
-	            sublist.add(nums[i]);
-	            backtrack(nums, result , sublist, i + 1);
-	            sublist.remove(sublist.size() - 1);
-	        }
-	    }
+
+		public List<List<Integer>> subsets(int[] nums) {
+			List<List<Integer>> result = new ArrayList<List<Integer>>();
+			List<Integer> sublist = new ArrayList<Integer>();
+			if(nums.length==0)
+				return result ;
+			backtrack(nums,result,sublist,0);
+			return result;
+		}
+
+		public void backtrack(int[] nums, List<List<Integer>> result, List<Integer> sublist, int start){
+			result.add(new ArrayList<Integer>(sublist)); //here is the catch in this prob
+
+			for(int i = start; i < nums.length; i++){
+				sublist.add(nums[i]);
+				backtrack(nums, result , sublist, i + 1);
+				sublist.remove(sublist.size() - 1);
+			}
+		}
 	}
 
 }
