59547ae02d037e8ebf702caa4371aacdf52ecfdd
partha biswas <biswas.partha1@gmail.com>
2024-03-02 15:33:49 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/HandsOfStraight.java
+++ problemSolving/src/main/java/com/partha/leetcode/HandsOfStraight.java
@@ -29,33 +29,27 @@ public class HandsOfStraight {
 	//https://www.youtube.com/watch?v=amnrMCVd2YI
 	private static class Solution1 {
 	    public boolean isNStraightHand(int[] hand, int groupSize) {
-	        if(hand.length%groupSize!=0)
+	        if (hand.length % groupSize != 0)
 	            return false;
-	        
-	        //counting occurances
-	        Map<Integer,Integer> map = new HashMap();
-	        IntStream.range(0,hand.length).forEach(i -> {
-	            map.put(hand[i], map.getOrDefault(hand[i],0)+1);
-	        });
-	        
-	        PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+	        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	        for (int card : hand)
+	            map.put(card, map.getOrDefault(card, 0) + 1);
+
+	        PriorityQueue<Integer> queue = new PriorityQueue<Integer>();
 	        queue.addAll(map.keySet());
 
-	        while(!queue.isEmpty()){
-	            int first = queue.peek();
-	            for(int i=first;i<first+groupSize;i++){
-	                
-	                //checking if the element is avaialable or not
-	                if(map.get(i)==null) 
+	        while (!queue.isEmpty()) {
+	            int firstKey = queue.peek();
+	            for (int i = firstKey; i < firstKey + groupSize; i++) {
+	                if (!map.containsKey(i)) // checking if the element is avaialable or not
 	                    return false;
 
-	                //getting count if available
-	                int count = map.get(i);
-	                map.put(i, --count); //decrementing the availability
+	                int count = map.get(i); // getting count if available
+	                map.put(i, --count); // decrementing the availability
 
-	                //if count after decrement is 0 then we have to pop from MinHeap
-	                if(count==0){
-	                    if(i!=queue.peek()){
+	                if (count == 0) { //after decrementing if count is zero
+	                    if (i != queue.peek()) { //if not top-element-count!=0 and subsequent-element-count==0 not possible
 	                        return false;
 	                    }
 	                    queue.poll();
@@ -69,36 +63,31 @@ public class HandsOfStraight {
 	
 	//same algo but using treemap as to combine the MinHeap and Map
 	//however this takes more time and execution memory.
-	private static class Solution {
+	private static class Solution2 {
 	    public boolean isNStraightHand(int[] hand, int groupSize) {
 	        if(hand.length%groupSize!=0)
-	            return false;
-	        
-	        //counting occurances
-	        TreeMap<Integer,Integer> map = new TreeMap();
-	        IntStream.range(0,hand.length).forEach(i -> {
-	            map.put(hand[i], map.getOrDefault(hand[i],0)+1);
-	        });
-	        
-	        while(map.size()>0){
-	            int first = map.firstKey();
-	            for(int i=first;i<first+groupSize;i++){
-	               
-	               if(!map.containsKey(i))
-	                    return false;
-	             
-	                int count = map.get(i); //get availability
-	                count-- ; //decrementing availability
-	                if(count==0){
-	                   map.remove(i);
-	                }else{
-	                    map.put(i,count);
-	                }
+	             return false;
+
+	        TreeMap<Integer, Integer> map= new TreeMap<Integer,Integer>();
+	        for(int card: hand)
+	            map.put(card, map.getOrDefault(card, 0)+1);
+
+	        while(!map.isEmpty()){
+	            int firstKey = map.firstKey();
+	            for(int i=firstKey;i<firstKey + groupSize;i++){
+	                if(!map.containsKey(i)) //if the key is not present
+	                     return false;
+	                 int count = map.get(i);
+	                 if(count-1==0){
+	                     map.remove(i);
+	                 }else{
+	                     map.put(i, count-1);
+	                 }
 	            }
 	        }
 	        return true;
-	    }
-	}
+	     }
+	 }
 	
 
 }
