68a79db492a6e036bfda6e93e49121cd911099ed
partha biswas <biswas.partha1@gmail.com>
2024-03-04 13:27:40 +0530

    adding more problem

---
+++ problemSolving/src/main/java/com/partha/leetcode/FindTheDuplicateNumber.java
@@ -0,0 +1,35 @@
+package com.partha.leetcode;
+
+/**
+ * https://leetcode.com/problems/find-the-duplicate-number
+ */
+public class FindTheDuplicateNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	private static class Solution1 {
+		  public int findDuplicate(int[] nums) {
+		    int slow = nums[nums[0]];
+		    int fast = nums[nums[nums[0]]];
+
+		    while (slow != fast) {
+		      slow = nums[slow];
+		      fast = nums[nums[fast]];
+		    }
+
+		    slow = nums[0];
+
+		    while (slow != fast) {
+		      slow = nums[slow];
+		      fast = nums[fast];
+		    }
+
+		    return slow;
+		  }
+		}
+
+}
