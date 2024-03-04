05a52c9c6d8b30508fcec6bbe423a7cad81a9b3e
partha biswas <biswas.partha1@gmail.com>
2024-03-04 11:41:47 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/AddTwoNumbers.java
+++ problemSolving/src/main/java/com/partha/leetcode/AddTwoNumbers.java
@@ -34,8 +34,8 @@ public class AddTwoNumbers {
 	private static class Solution2 {
 		public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 			int carry = 0;
-			ListNode p, dummy = new ListNode(0);
-			p = dummy;
+			ListNode dummy = new ListNode(0);
+			ListNode current = dummy;
 			while (l1 != null || l2 != null || carry != 0) {
 				if (l1 != null) {
 					carry += l1.val;
@@ -45,9 +45,9 @@ public class AddTwoNumbers {
 					carry += l2.val;
 					l2 = l2.next;
 				}
-				p.next = new ListNode(carry%10);
+				current.next = new ListNode(carry%10);
 				carry /= 10;
-				p = p.next;
+				current = current.next;
 			}
 			return dummy.next;
 		}
