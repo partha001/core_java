bc6891a941f8241d30a7c5d00e37812b0aa7ac17
partha biswas <biswas.partha1@gmail.com>
2024-03-06 22:26:13 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/DeleteOperationForTwoStrings.java
+++ problemSolving/src/main/java/com/partha/leetcode/DeleteOperationForTwoStrings.java
@@ -47,31 +47,25 @@ public class DeleteOperationForTwoStrings {
 	 * tabulation method. practice this solution
 	 */
 	private static class Solution2 {
-		public int minDistance(String s, String t) {
-
-			int m = s.length(), n = t.length();
-			int[][] dp = new int[m + 1][n + 1];
-
-			for(int i = 0; i <= m; i++){
-				for(int j = 0; j <= n; j++){
-					if(i==0) {
-						dp[i][j] = j;
-					}
-					else if(j==0) {
-						dp[i][j]=i;
-					}
-					else if(s.charAt(i - 1) == t.charAt(j - 1))
-						dp[i][j] = dp[i - 1][j - 1];
-					else{
-						int deleteInS = dp[i - 1][j];
-						int deleteInT = dp[i][j - 1];
-						dp[i][j] = Math.min(deleteInS, deleteInT) + 1;
-					}
-				}
-			}
-
-			return dp[m][n];
-		}
+	    public int minDistance(String word1, String word2) {
+	        int rows = word1.length()+1; //size is +1 to accomodate empty strings
+	        int cols = word2.length()+1;
+	        int[][] dp= new int[rows][cols];
+	        for(int i=0;i<rows;i++){
+	            for(int j=0;j<cols;j++){
+	                if(i==0) //for first row
+	                    dp[i][j] = j;
+	                else if(j==0)  //for first column
+	                    dp[i][j] = i;
+	                else if(word1.charAt(i-1)==word2.charAt(j-1)){  //when match then top-left diagonal element
+	                    dp[i][j] = dp[i-1][j-1];
+	                }else{ //
+	                    dp[i][j] = Math.min( dp[i][j-1], dp[i-1][j]) +1;
+	                }
+	            }
+	        }
+	        return dp[rows-1][cols-1];
+	    }
 	}
 
 
