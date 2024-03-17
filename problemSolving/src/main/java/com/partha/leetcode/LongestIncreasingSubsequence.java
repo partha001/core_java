3977f1df459cc3e43d54e499c582ac9617750d10
partha biswas <biswas.partha1@gmail.com>
2024-03-17 18:55:53 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/LongestIncreasingSubsequence.java
+++ problemSolving/src/main/java/com/partha/leetcode/LongestIncreasingSubsequence.java
@@ -4,43 +4,66 @@ import java.util.Arrays;
 
 /**
  * https://leetcode.com/problems/longest-increasing-subsequence/
- * @author biswaspa
+ * program tutorial: https://www.youtube.com/watch?v=cjWnW0hdF1Y
  *
+ * @author biswaspa
  */
 public class LongestIncreasingSubsequence {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
-	
-	/**
-	 * /Time: O(n2) space: O(n)
-	 * @author biswaspa
-	 *
-	 */
-	private static class Solution {
+    }
 
-	    public int lengthOfLIS(int[] nums) {
+    /**
+     * /Time: O(n2) space: O(n)
+     *
+     * @author biswaspa
+     */
+    private static class Solution1 {
 
-	        int dp[] = new int[nums.length];
-	        Arrays.fill(dp, 1);
+        public int lengthOfLIS(int[] nums) {
 
-	        for (int i = 1; i < nums.length; i++) {
-	            for (int j = 0; j < i; j++) {
-	                if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
-	                    dp[i] = dp[j] + 1;
-	                }
-	            }
-	        }
+            int dp[] = new int[nums.length];
+            Arrays.fill(dp, 1);
 
-	        int max = 0;
-	        for (int i = 0; i < dp.length; i++)
-	            max = Math.max(max, dp[i]);
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                    }
+                }
+            }
 
-	        return max;
+            int max = 0;
+            for (int i = 0; i < dp.length; i++)
+                max = Math.max(max, dp[i]);
 
-	    }
-	}
+            return max;
+
+        }
+    }
+
+    /**
+     * same code as above written in more crisper way
+     */
+    private static class Solution2 {
+        public int lengthOfLIS(int[] nums) {
+
+            int dp[] = new int[nums.length];
+            Arrays.fill(dp, 1);
+
+            int max = 1;
+            for (int i = 1; i < nums.length; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        max = Math.max(max, dp[i]);
+                    }
+                }
+            }
+            return max;
+        }
+    }
 
 }
