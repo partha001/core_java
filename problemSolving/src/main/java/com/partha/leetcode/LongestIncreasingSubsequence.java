076191bec19e9a475f52c5d9bbc1a798ada741a7
partha biswas <biswas.partha1@gmail.com>
2024-03-18 01:09:29 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/LongestIncreasingSubsequence.java
+++ problemSolving/src/main/java/com/partha/leetcode/LongestIncreasingSubsequence.java
@@ -20,33 +20,6 @@ public class LongestIncreasingSubsequence {
      *
      * @author biswaspa
      */
-    private static class Solution1 {
-
-        public int lengthOfLIS(int[] nums) {
-
-            int dp[] = new int[nums.length];
-            Arrays.fill(dp, 1);
-
-            for (int i = 1; i < nums.length; i++) {
-                for (int j = 0; j < i; j++) {
-                    if (nums[j] < nums[i] && dp[j] + 1 > dp[i]) {
-                        dp[i] = dp[j] + 1;
-                    }
-                }
-            }
-
-            int max = 0;
-            for (int i = 0; i < dp.length; i++)
-                max = Math.max(max, dp[i]);
-
-            return max;
-
-        }
-    }
-
-    /**
-     * same code as above written in more crisper way
-     */
     private static class Solution2 {
         public int lengthOfLIS(int[] nums) {
 
