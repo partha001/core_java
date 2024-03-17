c5acc377e5507ab158ee5b28b5624ca8e95622e8
partha biswas <biswas.partha1@gmail.com>
2024-03-17 16:56:13 +0530

    adding find the duplicate number

--- problemSolving/src/main/java/com/partha/leetcode/FindTheDuplicateNumber.java
+++ problemSolving/src/main/java/com/partha/leetcode/FindTheDuplicateNumber.java
@@ -5,31 +5,56 @@ package com.partha.leetcode;
  */
 public class FindTheDuplicateNumber {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
-	
-	
-	private static class Solution1 {
-		  public int findDuplicate(int[] nums) {
-		    int slow = nums[nums[0]];
-		    int fast = nums[nums[nums[0]]];
+    }
 
-		    while (slow != fast) {
-		      slow = nums[slow];
-		      fast = nums[nums[fast]];
-		    }
 
-		    slow = nums[0];
+    private static class Solution1 {
+        public int findDuplicate(int[] nums) {
+            int slow = nums[nums[0]];
+            int fast = nums[nums[nums[0]]];
 
-		    while (slow != fast) {
-		      slow = nums[slow];
-		      fast = nums[fast];
-		    }
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
 
-		    return slow;
-		  }
-		}
+            slow = nums[0];
+
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+
+            return slow;
+        }
+    }
+
+    /**
+     * previous algo written in a different way
+     **/
+    private static class Solution2 {
+        public int findDuplicate(int[] nums) {
+            int slow = nums[0];
+            int fast = nums[0];
+
+            do {
+                slow = nums[slow];
+                fast = nums[nums[fast]];
+            }
+            while (slow != fast);
+
+            slow = nums[0];
+
+            while (slow != fast) {
+                slow = nums[slow];
+                fast = nums[fast];
+            }
+
+            return fast;
+        }
+    }
 
 }
