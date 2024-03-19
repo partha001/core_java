c9fbaa6adc1ce4bca0c0f99afa70dbb2045c6d2c
partha biswas <biswas.partha1@gmail.com>
2024-03-19 16:45:50 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/StudentAttendance1.java
+++ problemSolving/src/main/java/com/partha/leetcode/StudentAttendance1.java
@@ -2,71 +2,63 @@ package com.partha.leetcode;
 
 /**
  * https://leetcode.com/problems/student-attendance-record-i
- * @author biswaspa
  *
+ * @author biswaspa
  */
 public class StudentAttendance1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
 
-	private static class Solution {
-		public boolean checkRecord(String s) {
-			int absent=0;
-			int i=0;
-			while(i<s.length()){
-				char current = s.charAt(i);
-				if(current=='A'){ //absent case
-					absent++;
-					if(absent>=2)
-						return false;
-					i++;
-					continue;
-				}
-				else if(current=='P'){ //case present
-					i++;
-					continue;
-				}
-				else {	//case late
-					int lateCount=0;
-					while(i<s.length() && s.charAt(i)=='L'){
-						lateCount++;
-						i++;
-						if(lateCount>=3)
-							return false;
-					}
-				}
-			}
-			return true;
-		}
-	}
-	
-	
-	
-	
-	private static class Solution2 {
-	    public boolean checkRecord(String s) {
-	        int absent=0;
-	        String temp ="";
-	       for(char c:s.toCharArray()){
-	           if(c=='A')
-	                absent++;
-	            
-	            if(c=='L'){
-	                temp += 'L';
-	            }else{
-	                temp="";
-	            }
+    private static class Solution1 {
+        public boolean checkRecord(String s) {
+            int absent = 0;
+            int late = 0;
+            int i = 0;
+            while (i < s.length()) {
+                char c = s.charAt(i);
+                if (c == 'A') {
+                    absent++;
+                    late = 0; //resetting late count if not consecutively late
+                } else if (c == 'L') {
+                    late++;
+                } else {
+                    late = 0; //resetting late count if not consecutively late
+                }
 
-	            if(absent>=2 || temp.length()>=3)
-	                return false;
-	       } 
-	       return true;
-	    }
-	}
+                if (absent == 2 || late == 3)
+                    return false;
+
+                i++;
+            }
+            return true;
+        }
+    }
+
+
+    private static class Solution2 {
+        public boolean checkRecord(String s) {
+            int absent = 0;
+            String temp = "";
+            for (char c : s.toCharArray()) {
+                if (c == 'A')
+                    absent++;
+
+                if (c == 'L') {
+                    temp += 'L';
+                } else {
+                    temp = "";
+                }
+
+                if (absent >= 2 || temp.length() >= 3)
+                    return false;
+            }
+            return true;
+        }
+    }
 
 
 }
