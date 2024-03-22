1508d5d593298f5bdd13f7fd74706598af93bdc9
partha biswas <biswas.partha1@gmail.com>
2024-03-23 00:19:26 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/PowerOfTwo.java
+++ problemSolving/src/main/java/com/partha/leetcode/PowerOfTwo.java
@@ -2,44 +2,33 @@ package com.partha.leetcode;
 
 /**
  * https://leetcode.com/problems/power-of-two
- * @author biswaspa
  *
+ * @author biswaspa
  */
 public class PowerOfTwo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
+    }
+
+
+	private static class Solution1 {
+		public boolean isPowerOfTwo(int n) {
+			if (n <= 0)
+				return false;
+			return (n & (n - 1)) == 0;
+		}
 	}
-	
-	
-	class Solution1 {
-	    public boolean isPowerOfTwo(int n) {
-	        if(n<=0)
-	        {
-	            return false;
-	        }
-	        boolean ans= (n&(n-1))==0;
-	        return ans;
-	        
-	    }
-	}
-	
-	
-	class Solution {
-	    public boolean isPowerOfTwo(int n) {
-	        if (n<1) return false;
-	        if (n==1) return true;
-	        if (n%2!=0) return false;
-	        return isPowerOfTwo(n/2);
-	    }
-	}
-	// if(n<=0) return false;
-	        // while(n!=0){
-	        //     if(n==1) return true;
-	        //     if(n%2==0) n/=2;
-	        //     else return false;
-	        // }
-	        // return true;
+
+
+    private static class Solution2 {
+        public boolean isPowerOfTwo(int n) {
+            if (n < 1) return false;
+            if (n == 1) return true;
+            if (n % 2 != 0) return false;
+			return isPowerOfTwo(n / 2);
+        }
+    }
 
 }
