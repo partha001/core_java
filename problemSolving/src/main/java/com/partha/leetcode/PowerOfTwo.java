7aa00ee1dfe6edad4ae7576c98325e4fef47880d
partha biswas <biswas.partha1@gmail.com>
2024-03-23 00:23:52 +0530

    code cleanup

--- problemSolving/src/main/java/com/partha/leetcode/PowerOfTwo.java
+++ problemSolving/src/main/java/com/partha/leetcode/PowerOfTwo.java
@@ -13,21 +13,29 @@ public class PowerOfTwo {
     }
 
 
-	private static class Solution1 {
-		public boolean isPowerOfTwo(int n) {
-			if (n <= 0)
-				return false;
-			return (n & (n - 1)) == 0;
-		}
-	}
+    private static class Solution1 {
+        public boolean isPowerOfTwo(int n) {
+            if (n <= 0)
+                return false;
+            return (n & (n - 1)) == 0; //using bitwise and operation to check.
+        }
+    }
 
 
     private static class Solution2 {
+
+        /**
+         * recursively dividing by 2 till a value less than 2 is
+         * reached while checking the modular division !=0 at any given point of time.
+         *
+         * @param n
+         * @return
+         */
         public boolean isPowerOfTwo(int n) {
-            if (n < 1) return false;
+            if (n < 1) return false; // base
             if (n == 1) return true;
             if (n % 2 != 0) return false;
-			return isPowerOfTwo(n / 2);
+            return isPowerOfTwo(n / 2);
         }
     }
 
