35bcda6595406566896c7d756ee530d432473782
partha biswas <biswas.partha1@gmail.com>
2024-03-23 07:42:26 +0530

    adding FractionToRecurringDecimal

--- problemSolving/src/main/java/com/partha/leetcode/FractionToRecurringDecimal.java
+++ problemSolving/src/main/java/com/partha/leetcode/FractionToRecurringDecimal.java
@@ -1,5 +1,6 @@
 package com.partha.leetcode;
 
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,7 +16,7 @@ public class FractionToRecurringDecimal {
     private static class Solution1 {
         public String fractionToDecimal(int numerator, int denominator) {
             StringBuilder ans = new StringBuilder();
-            if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0))
+            if ((numerator < 0 && denominator > 0) || (numerator > 0 && denominator < 0)) //cases1: when the result is negative
                 ans.append("-");
 
             long num = (long) Math.abs((long) numerator);
@@ -24,27 +25,27 @@ public class FractionToRecurringDecimal {
             long quotient = num / den;
             long remainder = num % den;
 
-            ans.append(String.valueOf(quotient));
+            ans.append(String.valueOf(quotient)); //appending the value to the left of decimal
 
-            if (remainder == 0)
+            if (remainder == 0) //if remainder is zero then return
                 return ans.toString();
 
             ans.append(".");
 
-            Map<Long, Integer> map = new HashMap<>();
+            Map<Long, Integer> map = new HashMap<>(); //here the key=remainder and value= length of answer before doing division
 
-            while (remainder != 0) {
-                if (map.containsKey(remainder)) {
-                    int pos = map.get(remainder);
+            while (remainder != 0) { //keep dividing till remainder is not zero
+                if (map.containsKey(remainder)) {  //if the remainder is already found means its recurring
+                    int pos = map.get(remainder);  //hence we get the position and 1.add "(" at the position and then add ")" at the end
                     ans.insert(pos, "(");
                     ans.append(")");
                     return ans.toString();
                 } else {
-                    map.put(remainder, ans.length());
-                    remainder *= 10;
-                    quotient = remainder / den;
-                    remainder %= den;
-                    ans.append(String.valueOf(quotient));
+                    map.put(remainder, ans.length()); //first we put the remainder and answer length in the map.
+                    remainder *= 10;  //making remainder greater than denominator by multiplying with 10
+                    quotient = remainder / den; //now dividing the remainder with denominator
+                    ans.append(String.valueOf(quotient)); //append quotiient to answer
+                    remainder %= den; //updating remainder using moduler division
                 }
             }
             return ans.toString();
