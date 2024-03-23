7bf6f4e63414d8ccbdea52a884c5c147f288ac09
partha biswas <biswas.partha1@gmail.com>
2024-03-23 16:01:49 +0530

    adding more comment

--- problemSolving/src/main/java/com/partha/leetcode/CountPrime.java
+++ problemSolving/src/main/java/com/partha/leetcode/CountPrime.java
@@ -75,3 +75,14 @@ public class CountPrime {
     }
 
 }
+/**
+ *To test whether a number is prime or not, why do we have to test whether it is divisible only up to the square root of that number?
+ *
+ *
+ * If a number n is not a prime, it can be factored into two factors a and b:
+ * n = a * b
+ * Now a and b can't be both greater than the square root of n, since then the
+ * product a * b would be greater than sqrt(n) * sqrt(n) = n. So in any factorization of n,
+ * at least one of the factors must be less than or equal to the square root of n, and if
+ * we can't find any factors less than or equal to the square root, n must be a prime
+ */
