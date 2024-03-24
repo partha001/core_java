7a996e5e6df05554dc7b2814cfa5c90ab6bd12c8
partha biswas <biswas.partha1@gmail.com>
2024-03-24 20:30:37 +0530

    adding comment

--- problemSolving/src/main/java/com/partha/leetcode/PowerOfXN.java
+++ problemSolving/src/main/java/com/partha/leetcode/PowerOfXN.java
@@ -9,12 +9,17 @@ public class PowerOfXN {
 
     }
 
+    /**
+     * this method calculates the value recursively
+     * doing some calculation at each step. and then we
+     * have the the values defined for base cases
+     */
     private static class Solution {
         public double myPow(double x, int n) {
-            if (n==0) return 1;
-            else if (n%2==0) return myPow(x*x, n/2);
-            else if (n%2==1) return x*myPow(x, n-1);
-            return 1/myPow(x, -n);
+            if (n==0) return 1; //case1: value to be returned when n=0
+            else if (n%2==0) return myPow(x*x, n/2); //value to be returned when n is an even number
+            else if (n%2==1) return x*myPow(x, n-1); //value to be returned when n is  odd.
+            return 1/myPow(x, -n); //value to be returned when n is negative;
         }
     }
 }
