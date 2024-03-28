6451feecbae2d2b202a19f45e1851491c0ebad18
partha biswas <biswas.partha1@gmail.com>
2024-03-28 11:11:19 +0530

    code formatting

--- problemSolving/src/main/java/com/partha/leetcode/ShortestPathInBinaryMatrix.java
+++ problemSolving/src/main/java/com/partha/leetcode/ShortestPathInBinaryMatrix.java
@@ -72,44 +72,41 @@ public class ShortestPathInBinaryMatrix {
     are marking the grid[][]=1 so that its not visited again since by problem statement we are suppossed
     to visit only cells=0 . so ones a cell is visited we make it 1.
      */
-    private static class Solution2 {
+    private static class Solution {
         public int shortestPathBinaryMatrix(int[][] grid) {
 
-            int[][] dirs = new int[][]{{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+            int[][] dirs = new int[][]{{0,-1},{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1}};
 
-            if (grid.length == 0) {
+            if(grid.length == 0){
                 return 0;
             }
 
             int rows = grid.length;
             int cols = grid[0].length;
 
-            if (grid[0][0] == 1 || grid[rows - 1][cols - 1] == 1)
+            if(grid[0][0] == 1 || grid[rows-1][cols-1] == 1)
                 return -1;
 
 
             Queue<int[]> queue = new LinkedList<>();
-            queue.add(new int[]{0, 0});
-            grid[0][0] = 1;
+            queue.add(new int[]{0,0});
+            grid[0][0]=1; //this is to mark the cell as visited
             int path = 0;
 
-            while (!queue.isEmpty()) {
+            while(!queue.isEmpty()){
                 int size = queue.size();
                 path++; //increasing the path for each layer
-                for (int i = 0; i < size; i++) {
-                    int[] pair = queue.poll();
-                    int m = pair[0];
-                    int n = pair[1];
-                    grid[m][n] = 1;
+                for(int i = 0;i<size;i++){
+                    int [] pair = queue.poll();
 
-                    if (pair[0] == rows - 1 && pair[1] == cols - 1)  // final case when user reach the target
-                        return path;
+                    if(pair[0] == rows-1 && pair[1] == cols-1)  // final case when user reach the target
+                        return path ;
 
-                    for (int[] dir : dirs) {
-                        int x = m + dir[0];
-                        int y = n + dir[1];
-                        if (x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 0) {
-                            grid[x][y] = 1;
+                    for(int[] dir:  dirs){
+                        int x = pair[0] + dir[0];
+                        int y = pair[1] + dir[1];
+                        if(x>=0 && x<rows && y>=0 && y<cols && grid[x][y] == 0 ){
+                            grid[x][y]=1; //this is to mark the cell as visited
                             queue.add(new int[]{x, y});
                         }
                     }
