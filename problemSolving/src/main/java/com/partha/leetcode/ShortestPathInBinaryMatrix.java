bbbc61a65c55cb916616ec38ceef113611b02704
partha biswas <biswas.partha1@gmail.com>
2024-03-28 11:07:50 +0530

    code formatting

--- problemSolving/src/main/java/com/partha/leetcode/ShortestPathInBinaryMatrix.java
+++ problemSolving/src/main/java/com/partha/leetcode/ShortestPathInBinaryMatrix.java
@@ -5,67 +5,119 @@ import java.util.Queue;
 
 /**
  * https://leetcode.com/problems/shortest-path-in-binary-matrix/
- * @author biswaspa
  *
+ * @author biswaspa
  */
 public class ShortestPathInBinaryMatrix {
 
-	public static void main(String[] args) {
-		int[][] grid = new int[][] {{0,0,0},{1,1,0},{1,1,0}};
-		System.out.println(new Solution1().shortestPathBinaryMatrix(grid));
-	}
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{{0, 0, 0}, {1, 1, 0}, {1, 1, 0}};
+        System.out.println(new Solution1().shortestPathBinaryMatrix(grid));
+    }
 
-	/**
-	 * @description abhiske's solution
-	 * @author partha
-	 *
-	 */
-	private static class Solution1 {
-		int r[] = new int[] { -1, -1, -1, 0, 0, 1, 1, 1 }; // co-ordinates
-		int c[] = new int[] { -1, 0, 1, -1, 1, -1, 0, 1 }; 
+    /**
+     * @author partha
+     * @description
+     */
+    private static class Solution1 {
+        public int shortestPathBinaryMatrix(int[][] grid) {
 
-		public int shortestPathBinaryMatrix(int[][] grid) {
-			if(grid.length == 0){
-				return 0;
-			}
+            int[][] dirs = new int[][]{{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
 
-			int n = grid.length;
-			int m = grid[0].length;
+            if (grid.length == 0) {
+                return 0;
+            }
 
-			boolean vis[][] = new boolean[n][m];
+            int rows = grid.length;
+            int cols = grid[0].length;
 
-			if(grid[0][0] == 1 || grid[n-1][m-1] == 1) 
-				return -1;
+            boolean vis[][] = new boolean[rows][cols];
+
+            if (grid[0][0] == 1 || grid[rows - 1][cols - 1] == 1)
+                return -1;
 
 
-			Queue<int[]> queue = new LinkedList<>();
-			queue.add(new int[]{0,0});
+            Queue<int[]> queue = new LinkedList<>();
+            queue.add(new int[]{0, 0});
 
-			vis[0][0] = true;
-			int path = 0;
+            vis[0][0] = true;
+            int path = 0;
 
-			while(!queue.isEmpty()){
-				int size = queue.size();
-				path++; //increasing the path for each layer
-				for(int i = 0;i<size;i++){
-					int [] pair = queue.poll();
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                path++; //increasing the path for each layer
+                for (int i = 0; i < size; i++) {
+                    int[] pair = queue.poll();
 
-					if(pair[0] == n-1 && pair[1] == m-1)  // final case when user reach the target
-						return path ;
+                    if (pair[0] == rows - 1 && pair[1] == cols - 1)  // final case when user reach the target
+                        return path;
 
-					for(int k  = 0;k<8; k++){
-						int x = pair[0] + r[k];
-						int y = pair[1] + c[k];
-						if(x>=0 && x<n && y>=0 && y<m && grid[x][y] == 0 && vis[x][y] == false){
-							vis[x][y] = true;
-							queue.add(new int[]{x, y});
-						}
-					}
-				}	           
-			}
-			return -1;
-		}
-	}
+                    for (int[] dir : dirs) {
+                        int x = pair[0] + dir[0];
+                        int y = pair[1] + dir[1];
+                        if (x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 0 && vis[x][y] == false) {
+                            vis[x][y] = true;
+                            queue.add(new int[]{x, y});
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+
+
+    /*
+    same solution as above : however here instead of making use of vis[][] to track cells visited we
+    are marking the grid[][]=1 so that its not visited again since by problem statement we are suppossed
+    to visit only cells=0 . so ones a cell is visited we make it 1.
+     */
+    private static class Solution2 {
+        public int shortestPathBinaryMatrix(int[][] grid) {
+
+            int[][] dirs = new int[][]{{0, -1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {1, 1}, {1, 0}, {1, -1}};
+
+            if (grid.length == 0) {
+                return 0;
+            }
+
+            int rows = grid.length;
+            int cols = grid[0].length;
+
+            if (grid[0][0] == 1 || grid[rows - 1][cols - 1] == 1)
+                return -1;
+
+
+            Queue<int[]> queue = new LinkedList<>();
+            queue.add(new int[]{0, 0});
+            grid[0][0] = 1;
+            int path = 0;
+
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                path++; //increasing the path for each layer
+                for (int i = 0; i < size; i++) {
+                    int[] pair = queue.poll();
+                    int m = pair[0];
+                    int n = pair[1];
+                    grid[m][n] = 1;
+
+                    if (pair[0] == rows - 1 && pair[1] == cols - 1)  // final case when user reach the target
+                        return path;
+
+                    for (int[] dir : dirs) {
+                        int x = m + dir[0];
+                        int y = n + dir[1];
+                        if (x >= 0 && x < rows && y >= 0 && y < cols && grid[x][y] == 0) {
+                            grid[x][y] = 1;
+                            queue.add(new int[]{x, y});
+                        }
+                    }
+                }
+            }
+            return -1;
+        }
+    }
 
 
 }
