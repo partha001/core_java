aa6ece5e2b59bcde15a996772d9e2df6fcb8750c
partha biswas <biswas.partha1@gmail.com>
2024-04-07 17:22:34 +0530

    code formatting

--- problemSolving/src/main/java/com/partha/leetcode/PalindromePartitioning.java
+++ problemSolving/src/main/java/com/partha/leetcode/PalindromePartitioning.java
@@ -1,85 +1,85 @@
 package com.partha.leetcode;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * https://leetcode.com/problems/palindrome-partitioning/
- * @author partha
  *
+ * @author partha
  */
 public class PalindromePartitioning {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
 
-	}
-
-
-	//using backtracking
-	private static class Solution {
-		public List<List<String>> partition(String s) {
-			List<List<String>> result = new ArrayList<List<String>>();
-			dfs(0, result, new ArrayList<String>(), s);
-			return result;
-		}
-
-		void dfs(int start, List<List<String>> result, List<String> currentList, String s) {
-			//ie. when the full length of string is covered then adding whatever is there in currentList to result
-			if (start >= s.length()){ 
-				result.add(new ArrayList<String>(currentList));
-				return;
-			}
+    }
 
 
-			for (int end = start; end < s.length(); end++) {
-				if (isPalindrome(s, start, end)) {
-					// add current substring in the currentList
-					currentList.add(s.substring(start, end + 1));
-					dfs(end + 1, result, currentList, s);
-					// backtrack and remove the current substring from currentList
-					currentList.remove(currentList.size() - 1);
-				}
-			}
-		}
+    //using backtracking
+    private static class Solution {
+        public List<List<String>> partition(String s) {
+            List<List<String>> result = new ArrayList<List<String>>();
+            dfs(0, result, new ArrayList<String>(), s);
+            return result;
+        }
 
-		boolean isPalindrome(String s, int low, int high) {
-			while (low < high) {
-				if (s.charAt(low++) != s.charAt(high--)) return false;
-			}
-			return true;
-		}
-	}
+        void dfs(int start, List<List<String>> result, List<String> currentList, String s) {
+            //ie. when the full length of string is covered then adding whatever is there in currentList to result
+            if (start >= s.length()) {
+                result.add(new ArrayList<String>(currentList));
+                return;
+            }
 
-	/**
-	 * backtracking + dp
-	 * https://www.youtube.com/watch?v=3jvWodd7ht0
-	 * @author biswa
-	 *
-	 */
-	//backtracking + dp
-	private static class Solution2 {
-		public List<List<String>> partition(String s) {
-			int len = s.length();
-			boolean[][] dp = new boolean[len][len];
-			List<List<String>> result = new ArrayList<>();
-			dfs(result, s, 0, new ArrayList<>(), dp);
-			return result;
-		}
 
-		void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
-			if (start >= s.length()) result.add(new ArrayList<>(currentList));
-			for (int end = start; end < s.length(); end++) {
-				if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) { //condition for palindrome
-					dp[start][end] = true;
-					currentList.add(s.substring(start, end + 1));
-					dfs(result, s, end + 1, currentList, dp);
-					currentList.remove(currentList.size() - 1);
-				}
-			}
-		}
-	}
+            for (int end = start; end < s.length(); end++) {
+                if (isPalindrome(s, start, end)) {
+                    // add current substring in the currentList
+                    currentList.add(s.substring(start, end + 1));
+                    dfs(end + 1, result, currentList, s);
+                    // backtrack and remove the current substring from currentList
+                    currentList.remove(currentList.size() - 1);
+                }
+            }
+        }
+
+        boolean isPalindrome(String s, int low, int high) {
+            while (low < high) {
+                if (s.charAt(low++) != s.charAt(high--)) return false;
+            }
+            return true;
+        }
+    }
+
+    /**
+     * backtracking + dp
+     * https://www.youtube.com/watch?v=3jvWodd7ht0
+     *
+     * @author biswa
+     */
+    //backtracking + dp
+    private static class Solution2 {
+        public List<List<String>> partition(String s) {
+            int len = s.length();
+            boolean[][] dp = new boolean[len][len];
+            List<List<String>> result = new ArrayList<>();
+            dfs(result, s, 0, new ArrayList<>(), dp);
+            return result;
+        }
+
+        void dfs(List<List<String>> result, String s, int start, List<String> currentList, boolean[][] dp) {
+            if (start >= s.length())
+                result.add(new ArrayList<>(currentList));
+            for (int end = start; end < s.length(); end++) {
+                if (s.charAt(start) == s.charAt(end) && (end - start <= 2 || dp[start + 1][end - 1])) { //condition for palindrome
+                    dp[start][end] = true;
+                    currentList.add(s.substring(start, end + 1));
+                    dfs(result, s, end + 1, currentList, dp);
+                    currentList.remove(currentList.size() - 1);
+                }
+            }
+        }
+    }
 
 
 }
