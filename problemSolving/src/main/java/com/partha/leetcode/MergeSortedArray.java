5557fcc35c6217cc12027e4a79fb730ddf671e29
Partha Biswas <biswas.partha1@gmail.com>
2025-06-28 09:16:11 +0530

    problemSolving: merge sorted array

--- problemSolving/src/main/java/com/partha/leetcode/MergeSortedArray.java
+++ problemSolving/src/main/java/com/partha/leetcode/MergeSortedArray.java
@@ -81,4 +81,28 @@ public class MergeSortedArray {
 	    }
 	}
 
+	/**
+	 * this is same as solution2 . just have written the code in a cleaner way
+	 */
+	private static class Solution3 {
+		public void merge(int[] nums1, int m, int[] nums2, int n) {
+			int k= m + n-1;
+			m--;
+			n--;
+			while(k>=0){
+				if(m>=0 && n>=0){
+					if(nums1[m]>=nums2[n]){
+						nums1[k--] = nums1[m--];
+					}else{
+						nums1[k--] = nums2[n--];
+					}
+				}else if(m>=0){
+					nums1[k--] = nums1[m--];
+				}else{
+					nums1[k--] = nums2[n--];
+				}
+			}
+		}
+	}
+
 }
